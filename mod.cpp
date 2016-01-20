2f3762e633b89a7260ed248c42336ac80ebdc98e
PabstMirror <pabstmirror@gmail.com>
2016-01-19 18:09:52 -0600

    Git Stuff

---
+++ mod.cpp
@@ -0,0 +1,12 @@
+name = "Advanced Combat Environment 3.4.2";
+picture = "logo_ace3_ca.paa";
+actionName = "GitHub";
+action = "https://github.com/acemod/ACE3";
+description = "ACE3 - Version 3.4.2";
+logo = "logo_ace3_ca.paa";
+logoOver = "logo_ace3_ca.paa";
+tooltip = "ACE3";
+tooltipOwned = "ACE3 Owned";
+overview = "Advanced Combat Environment 3, also known as ACE3, is a joint effort by the teams behind ACE2, AGM and CSE to improve the realism and authenticity of Arma 3.";
+author = "ACE3 Team";
+overviewPicture = "logo_ace3_ca.paa";
