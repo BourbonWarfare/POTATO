0d31ed3aece776d3c7a46d2fb4af538a68d4de14
PabstMirror <pabstmirror@gmail.com>
2016-05-25 16:30:36 -0500

    Help the blind - Close #41

--- addons/zeusMonitor/RscDisplayCurator.hpp
+++ addons/zeusMonitor/RscDisplayCurator.hpp
@@ -6,7 +6,7 @@ class RscDisplayCurator {
         class GVAR(acreSpectateButton): RscButton {
             idc = 777550;
             x = "safezoneX";
-            y = "safezoneY + safeZoneH - 12 * pixelH";
+            y = "safezoneY + safeZoneH - 14 * pixelH";
             w = "80 * pixelW";
             h = "12 * pixelH";
             sizeEx = "12 * pixelH";
@@ -16,10 +16,10 @@ class RscDisplayCurator {
         class GVAR(unitCount): RscText {
             idc = 777551;
             x = "safezoneX + 80 * pixelW";
-            y = "safezoneY + safeZoneH - 12 * pixelH";
+            y = "safezoneY + safeZoneH - 14 * pixelH";
             w = "80 * pixelW";
-            h = "12 * pixelH";
-            sizeEx = "12 * pixelH";
+            h = "14 * pixelH";
+            sizeEx = "14 * pixelH";
             text = "Sample";
         };
     };
