12845456adf4867d9954d47099ff4110d4a939ac
PabstMirror <pabstmirror@gmail.com>
2016-05-24 22:46:03 -0500

    MELB -> RHS compat

--- addons/miscFixes/config.cpp
+++ addons/miscFixes/config.cpp
@@ -11,6 +11,15 @@ class CfgPatches {
         VERSION_CONFIG;
     };
 
+    //MELB -> RHS Patch [Keep until August]:
+    class MELB {
+        units[] = {"MELB_H6M","MELB_AH6M_L","MELB_AH6M_M","MELB_AH6M_H","MELB_MH6M"};
+        weapons[] = {};
+        requiredVersion = 0.1;
+        requiredAddons[] = {"rhsusf_c_melb"};
+    };
+
+
     //CUP/AiA Terrain Dummies:
     class CUP_Structures_Config {
         units[] = {};
@@ -58,5 +67,28 @@ class CfgVehicles {
     class LandVehicle;
     class StaticWeapon: LandVehicle {
         insideSoundCoef = 0; //ACRE uses this for vehicle sound attenuation (makes it easy to hear mortar gunners)
+    };
+
+
+    //MELB -> RHS Patch:
+    class RHS_MELB_H6M;
+    class MELB_H6M: RHS_MELB_H6M {
+        displayName = "OH-6M [Replace With RHS Version]";
+    };
+    class RHS_MELB_AH6M_L;
+    class MELB_AH6M_L: RHS_MELB_AH6M_L {
+        displayName = "AH-6M-L [Replace With RHS Version]";
+    };
+    class RHS_MELB_AH6M_M;
+    class MELB_AH6M_M: RHS_MELB_AH6M_M {
+        displayName = "AH-6M-M [Replace With RHS Version]";
+    };
+    class RHS_MELB_AH6M_H;
+    class MELB_AH6M_H: RHS_MELB_AH6M_H {
+        displayName = "AH-6M-H [Replace With RHS Version]";
+    };
+    class RHS_MELB_MH6M;
+    class MELB_MH6M: RHS_MELB_MH6M {
+        displayName = "MH-6M [Replace With RHS Version]";
     };
 };
