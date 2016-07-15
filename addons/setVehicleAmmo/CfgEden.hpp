c36edddf822135c709ec419a5e0b6cdaccb8d918
PabstMirror <pabstmirror@gmail.com>
2016-07-14 22:10:54 -0500

    Fix veh ammo for 1.62

--- addons/setVehicleAmmo/CfgEden.hpp
+++ addons/setVehicleAmmo/CfgEden.hpp
@@ -17,38 +17,38 @@ class Cfg3DEN {
             attributeLoad = QUOTE([ARR_2(_this, _value)] call FUNC(attributeLoad));
             attributeSave = QUOTE([_this] call FUNC(attributeSave));
 
-            h = "(15 * 	5 + 1) * (pixelH * pixelGrid * 0.25)";
+            h = "(15 * 5 + 1) * (pixelH * pixelGrid * 0.5)";
             class Controls: Controls {
                 class Type: ctrlToolbox {
                     idc = 100;
-                    x = "5 * (pixelW * pixelGrid * 0.25)";
-                    y = "0 * 	5 * (pixelH * pixelGrid * 0.25)";
-                    w = "(	48 + 	82 - 	5) * (pixelW * pixelGrid * 0.25)";
-                    h = "1 * 	5 * (pixelH * pixelGrid * 0.25)";
+                    x = "5 * (pixelW * pixelGrid * 0.5)";
+                    y = "0 * 	5 * (pixelH * pixelGrid * 0.5)";
+                    w = "(	48 + 	82 - 	5) * (pixelW * pixelGrid * 0.5)";
+                    h = "1 * 	5 * (pixelH * pixelGrid * 0.5)";
                     rows = 1;
                     columns = 2;
                     strings[] = {"Default","Custom"};
                 };
                 class ListBackground: ctrlStatic {
-                    x = "5 * (pixelW * pixelGrid * 0.25)";
-                    y = "1 * 	5 * (pixelH * pixelGrid * 0.25)";
-                    w = "(	48 + 	82 - 	5) * (pixelW * pixelGrid * 0.25)";
-                    h = "13 * 	5 * (pixelH * pixelGrid * 0.25)";
+                    x = "5 * (pixelW * pixelGrid * 0.5)";
+                    y = "1 * 	5 * (pixelH * pixelGrid * 0.5)";
+                    w = "(	48 + 	82 - 	5) * (pixelW * pixelGrid * 0.5)";
+                    h = "13 * 	5 * (pixelH * pixelGrid * 0.5)";
                     colorBackground[] = {1,1,1,0.1};
                 };
                 class List: ctrlTree {
                     idc = 101;
-                    x = "5 * (pixelW * pixelGrid * 0.25)";
-                    y = "1 * 	5 * (pixelH * pixelGrid * 0.25)";
-                    w = "(	48 + 	82 - 	5) * (pixelW * pixelGrid * 0.25)";
-                    h = "13 * 	5 * (pixelH * pixelGrid * 0.25)";
+                    x = "5 * (pixelW * pixelGrid * 0.5)";
+                    y = "1 * 	5 * (pixelH * pixelGrid * 0.5)";
+                    w = "(	48 + 	82 - 	5) * (pixelW * pixelGrid * 0.5)";
+                    h = "13 * 	5 * (pixelH * pixelGrid * 0.5)";
                 };
                 class ammoSlider: ctrlXSliderH {
                     idc = 102;
-                    x = "5 * (pixelW * pixelGrid * 0.25)";
-                    y = "14 * 	5 * (pixelH * pixelGrid * 0.25)";
-                    w = "(	48 + 	82 - 	5) * (pixelW * pixelGrid * 0.25)";
-                    h = "1 * 	5 * (pixelH * pixelGrid * 0.25)";
+                    x = "5 * (pixelW * pixelGrid * 0.5)";
+                    y = "14 * 	5 * (pixelH * pixelGrid * 0.5)";
+                    w = "(	48 + 	82 - 	5) * (pixelW * pixelGrid * 0.5)";
+                    h = "1 * 	5 * (pixelH * pixelGrid * 0.5)";
                 };
             };
         };
