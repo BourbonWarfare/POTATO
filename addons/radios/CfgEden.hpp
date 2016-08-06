dccc5912ecc52901fd285711b4977d97e73a5505
AACO <mruffne2@gmail.com>
2016-08-05 23:24:12 -0500

    fix icon path from ACRE update (for real this time)

--- addons/radios/CfgEden.hpp
+++ addons/radios/CfgEden.hpp
@@ -31,8 +31,8 @@ class GVAR(radioSelect): ctrlToolbox {
     rows = 1;
     columns = 3;
     strings[] = {
-        "\idi\clients\acre\addons\sys_prc343\Data\static\343_icon.paa",
-        "\idi\clients\acre\addons\sys_prc148\Data\static\148_icon.paa",
+        "\idi\clients\acre\addons\sys_prc343\Data\static\prc343_icon.paa",
+        "\idi\clients\acre\addons\sys_prc148\Data\static\prc148_icon.paa",
         "\idi\clients\acre\addons\sys_prc117f\Data\PRC117F_ico.paa"
     };
     tooltips[] = {
