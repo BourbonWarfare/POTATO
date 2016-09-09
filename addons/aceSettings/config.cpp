655c3c9ba02ebef22d9541861307ad90b9f8a1d8
PabstMirror <pabstmirror@gmail.com>
2016-09-09 15:08:21 -0500

    Settings - sort and add fatigue/medical_ai

--- addons/aceSettings/config.cpp
+++ addons/aceSettings/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"potato_core", "ace_laserPointer", "ace_map", "ace_zeus", "ace_hearing", "ace_overheating", "ace_gforces", "ace_goggles", "ace_frag", "ace_medical"};
+        requiredAddons[] = {"potato_core","ace_laserPointer","ace_map","ace_zeus","ace_hearing","ace_overheating","ace_gforces","ace_goggles","ace_frag","ace_medical","ace_medical_ai","ace_advanced_fatigue"};
         author = "Potato";
         authors[] = {"PabstMirror"};
         authorUrl = "https://github.com/BourbonWarfare/POTATO";
@@ -16,17 +16,31 @@ class CfgPatches {
 
 // ACE Settings: (see http://forums.bourbonwarfare.com/viewtopic.php?f=8&t=2026)
 class ACE_Settings {
-
-    // Lets force this at 2x for now (from 6x in missions) while adjusting to new basic medical
-    class ace_medical_bleedingCoefficient {
-        typeName = "NUMBER";
-        value = 2;
-        force = 1; // This overrides mission setting/module!!!
+    class ace_advanced_fatigue_performanceFactor {
+        typeName = "SCALAR";
+        value = 1.5;  
     };
-
-
-    //Daylight laser pointer:
-    class ace_laserPointer_enabled {
+    class ace_advanced_fatigue_recoveryFactor {
+        typeName = "SCALAR";
+        value = 1.5;  
+    };
+    class ace_frag_maxTrack {
+        typeName = "SCALAR";
+        value = 2;
+    };
+    class ace_gforces_enabledFor {
+        typeName = "SCALAR";
+        value = 0;
+    };
+    class ace_goggles_effects { //Disable rotor-wash and prone dust effects
+        typeName = "SCALAR";
+        value = 1;
+    };
+    class ace_hearing_autoAddEarplugsToUnits {
+        typeName = "BOOL";
+        value = 0;
+    };
+    class ace_laserPointer_enabled { // Daylight laser pointer
         typeName = "BOOL";
         value = 0;
     };
@@ -34,11 +48,16 @@ class ACE_Settings {
         typeName = "NUMBER";
         value = 1; //SIDE
     };
-    class ace_zeus_autoAddObjects {
-        typeName = "BOOL";
+    class ace_medical_bleedingCoefficient {
+        typeName = "NUMBER";
+        value = 2;
+        force = 1; // This overrides mission setting/module!!!
+    };
+    class ace_medical_ai_enabledFor {
+        typeName = "SCALAR";
         value = 1;
     };
-    class ace_hearing_autoAddEarplugsToUnits {
+    class ace_overheating_enabled {
         typeName = "BOOL";
         value = 0;
     };
@@ -50,7 +69,7 @@ class ACE_Settings {
         typeName = "SCALAR";
         value = 0;
     };
-    class ace_overheating_enabled {
+    class ace_respawn_removeDeadBodiesDisconnected {
         typeName = "BOOL";
         value = 0;
     };
@@ -64,20 +83,8 @@ class ACE_Settings {
         value = 0;
         force = 1;
     };
-    class ace_respawn_removeDeadBodiesDisconnected {
+    class ace_zeus_autoAddObjects {
         typeName = "BOOL";
-        value = 0;
-    };
-    class ace_goggles_effects { //Disable rotor-wash and prone dust effects
-        typeName = "SCALAR";
         value = 1;
-    };
-    class ace_gforces_enabledFor {
-        typeName = "SCALAR";
-        value = 0;
-    };
-    class ace_frag_maxTrack {
-        typeName = "SCALAR";
-        value = 2;
     };
 };
