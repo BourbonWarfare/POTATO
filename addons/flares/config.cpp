7f6bb5bf09f950c5a878a63314c17aab24424264
PabstMirror <pabstmirror@gmail.com>
2016-10-06 18:09:43 -0500

    Increase illumination from vanilla and rhs flares

---
+++ addons/flares/config.cpp
@@ -0,0 +1,16 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"potato_core", "rhs_c_weapons"};
+        author = "Potato";
+        authors[] = {"PabstMirror"};
+        authorUrl = "https://github.com/BourbonWarfare/POTATO";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgAmmo.hpp"
