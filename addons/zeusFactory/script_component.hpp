43ef01a9b1268b139049f804ec3be0002216dd1e
PabstMirror <pabstmirror@gmail.com>
2017-02-02 22:11:51 -0600

    v18.6

--- addons/zeusFactory/script_component.hpp
+++ addons/zeusFactory/script_component.hpp
@@ -2,8 +2,8 @@
 #include "\z\potato\addons\core\script_mod.hpp"
 
 #define DEBUG_MODE_FULL
-#define DISABLE_COMPILE_CACHE
-#define ENABLE_PERFORMANCE_COUNTERS
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
 
 #ifdef DEBUG_ENABLED_ZEUSFACTORY
     #define DEBUG_MODE_FULL
