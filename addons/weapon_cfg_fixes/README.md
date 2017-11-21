5d79a35663960d3a4d15dbf3144686995d43dc31
Brandon Danyluk <BrandonDany@hotmail.co.uk>
2017-11-20 22:07:16 -0700

    init commit

---
+++ addons/weapon_cfg_fixes/README.md
@@ -0,0 +1,4 @@
+# Weapon Cfg Fixes
+
+Standardizes AI range's on all weapons. Script provided to keep maintenence easy
+
---
+++ addons/weapon_cfg_fixes/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT weapons
+#include "\z\potato\addons\core\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_WEAPONS
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_WEAPONS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_VZ99
+#endif
+
+#include "\z\potato\addons\core\script_macros.hpp"
+
