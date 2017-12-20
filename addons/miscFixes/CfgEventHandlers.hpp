c2c6ba92b492f2c16d9218fa1a485d89d16d65f2
Mark Ruffner <mruffne2@gmail.com>
2017-12-19 18:45:45 -0600

    BI has fixed in 1.80 hotfix (#201)
    Revert "Add dummy var to prevent loadCoef enum error"
    
    This reverts commit bdb01a1be2565549f76ee426cce1066ad1907777.

--- addons/miscFixes/CfgEventHandlers.hpp
+++ addons/miscFixes/CfgEventHandlers.hpp
@@ -1,9 +1,3 @@
-class Extended_PreInit_EventHandlers {
-    class ADDON {
-        clientInit = QUOTE(call COMPILE_FILE(XEH_fixEnumError));
-    };
-};
-
 class Extended_PostInit_EventHandlers {
     class ADDON {
         init = QUOTE(call COMPILE_FILE(XEH_kilTracker));
