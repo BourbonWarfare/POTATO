bd761c2c70f259f8f6fb214fb6c2ccd457b9b8af
PabstMirror <pabstmirror@gmail.com>
2018-04-21 15:37:25 -0500

    Auto set author

--- addons/missionTest/Cfg3DEN.hpp
+++ addons/missionTest/Cfg3DEN.hpp
@@ -2,6 +2,7 @@ class Cfg3DEN {
     class EventHandlers {
         class ADDON {
             onMissionSave = QUOTE(call compile preprocessFileLineNumbers QUOTE(QPATHTOF(functions\fnc_onMissionSave.sqf)));
+            onMissionLoad = QUOTE(call compile preprocessFileLineNumbers QUOTE(QPATHTOF(functions\fnc_onMissionLoad.sqf)));
         };
     };
 };
