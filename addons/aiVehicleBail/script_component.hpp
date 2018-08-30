363911194de6ce3233b4fda9ddc61ecc63aaeb1f
Brandon-PC\Brandon <brandondanyluk366@gmail.com>
2018-08-30 05:54:32 -0600

    init commit

---
+++ addons/aiVehicleBail/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT aiVehicleBail
+#include "\z\potato\addons\core\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+#define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_AI_VEHICLE_BAIL
+    #define DEBUG_MODE_FULL
+#endif
+
+#include "\z\potato\addons\core\script_macros.hpp"
+#include "script_macros.hpp"
