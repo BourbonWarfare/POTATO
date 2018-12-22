c247230090c878363fb1d28fc202230a4568e4b7
PabstMirror <pabstmirror@gmail.com>
2018-12-21 20:02:37 -0600

    Fix Caribou

---
+++ addons/miscFixes/CfgWorlds.hpp
@@ -0,0 +1,7 @@
+class CfgWorlds {
+    class CAWorld;
+    class Chernarus: CAWorld {};
+    class Caribou: Chernarus {
+        newRoadsShape = "";
+    };
+};
--- addons/miscFixes/config.cpp
+++ addons/miscFixes/config.cpp
@@ -20,6 +20,7 @@ class CfgPatches {
             "rhs_c_weapons", "rhsusf_c_weapons",
             "CUP_Weapons_AK", "hlcweapons_G36",
             "rhsgref_c_troops"
+            ,"Chernarus", "Caribou"
         };
         author = "Potato";
         authors[] = {"PabstMirror", "AACO"};
@@ -47,5 +48,6 @@ class RscChatListDefault {
 #include "CfgVehicles.hpp"
 #include "CfgWeapons.hpp"
 #include "CfgFontFamilies.hpp"
+#include "CfgWorlds.hpp"
 
 #endif
