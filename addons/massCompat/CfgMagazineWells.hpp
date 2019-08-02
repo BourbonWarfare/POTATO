09c5d4a204d30cae53fb4153dd5307cf23236e40
PabstMirror <pabstmirror@gmail.com>
2019-08-02 16:16:15 -0500

    Add some temp magwells for compat

---
+++ addons/massCompat/CfgMagazineWells.hpp
@@ -0,0 +1,15 @@
+class CfgMagazineWells {
+
+    class CBA_545x39_AK {
+        GVAR(RHS_temp)[] = {"rhs_30Rnd_545x39_AK","rhs_30Rnd_545x39_7N6_AK","rhs_30Rnd_545x39_AK_no_tracers","rhs_30Rnd_545x39_7N6_green_AK","rhs_30Rnd_545x39_AK_green","rhs_30Rnd_545x39_AK_plum_green","rhs_30Rnd_545x39_7U1_AK","rhs_30Rnd_545x39_7N6M_AK","rhs_30Rnd_545x39_7N6M_green_AK","rhs_30Rnd_545x39_7N6M_plum_AK","rhs_30Rnd_545x39_7N10_AK","rhs_30Rnd_545x39_7N10_plum_AK","rhs_30Rnd_545x39_7N10_camo_AK","rhs_30Rnd_545x39_7N10_desert_AK","rhs_30Rnd_545x39_7N22_AK","rhs_30Rnd_545x39_7N22_plum_AK","rhs_30Rnd_545x39_7N22_camo_AK","rhs_30Rnd_545x39_7N22_desert_AK","rhs_30Rnd_545x39_7N10_2mag_AK","rhs_30Rnd_545x39_7N10_2mag_plum_AK","rhs_30Rnd_545x39_7N10_2mag_camo_AK","rhs_30Rnd_545x39_7N10_2mag_desert_AK","rhs_45Rnd_545x39_AK","rhs_45Rnd_545x39_7N6_AK","rhs_45Rnd_545x39_7N10_AK","rhs_45Rnd_545x39_7N22_AK","rhs_45Rnd_545x39_AK_green","rhs_45Rnd_545x39_7U1_AK"};
+    };
+    class CBA_545x39_RPK {
+        GVAR(RHS_temp)[] = {"rhs_45Rnd_545x39_AK","rhs_45Rnd_545x39_7N6_AK","rhs_45Rnd_545x39_7N10_AK","rhs_45Rnd_545x39_7N22_AK","rhs_45Rnd_545x39_AK_green","rhs_45Rnd_545x39_7U1_AK"};
+    };
+    class CBA_762x51_FAL {
+        GVAR(CUP_temp)[] = {"CUP_20rnd_762x51_FNFAL_M"};
+    };
+    class CBA_556x45_MINIMI {
+        GVAR(CUP_temp)[] = {"CUP_200rnd_TE4_Red_Tracer_556x45_m249","CUP_200rnd_TE4_Yellow_Tracer_556x45_m249","CUP_200rnd_TE4_Green_Tracer_556x45_m249","CUP_200rnd_TE1_Red_Tracer_556x45_m249","CUP_100rnd_TE4_Red_Tracer_556x45_m249","CUP_100rnd_TE4_Yellow_Tracer_556x45_m249","CUP_100rnd_TE4_Green_Tracer_556x45_m249","CUP_200rnd_TE4_Red_Tracer_556x45_L110A1","CUP_200rnd_TE4_Yellow_Tracer_556x45_L110A1","CUP_200rnd_TE4_Green_Tracer_556x45_L110A1"};
+    };
+};
