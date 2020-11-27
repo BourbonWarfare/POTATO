7157b01ea25b4696d6aef0bf4298c3f368087f2d
Alablm <aidan197@hotmail.com>
2020-11-26 20:16:54 -0500

    Unit side fix
    Corrects all units spawning as blufor from the zues editor.
    
    Note: Corrently opfor infantry is broken in Zeus.

--- addons/units/CfgGroups.hpp
+++ addons/units/CfgGroups.hpp
@@ -1,15 +1,15 @@
 class CfgGroups {
     #include "groups\groupDef.hpp"
     #define CLASS_MACRO(unit) USMC(unit)
-    GROUP_DEF(WEST,"Potato: West",w)
+    GROUP_DEF(WEST,"Potato: West",w,1)
 
     #undef CLASS_MACRO
     #undef FACTION_LETTER
     #define CLASS_MACRO(unit) MSV(unit)
-    GROUP_DEF(EAST,"Potato: East",e)
+    GROUP_DEF(EAST,"Potato: East",e,0)
 
     #undef CLASS_MACRO
     #undef FACTION_LETTER
     #define CLASS_MACRO(unit) AIR(unit)
-    GROUP_DEF(Indep,"Potato: Indy",i)
+    GROUP_DEF(Indep,"Potato: Indy",i,2)
 };
