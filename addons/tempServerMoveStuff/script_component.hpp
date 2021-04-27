b21a8d2f64643a563d74c6bd7b550e1c86982f9a
PabstMirror <pabstmirror@gmail.com>
2021-04-27 18:59:27 -0500

    Cleanup server move stuff

--- addons/tempServerMoveStuff/script_component.hpp
+++
@@ -1,8 +0,0 @@
-#define COMPONENT tempServerMoveStuff
-#include "\z\potato\addons\core\script_mod.hpp"
-
-// #define DEBUG_MODE_FULL
-// #define DISABLE_COMPILE_CACHE
-// #define ENABLE_PERFORMANCE_COUNTERS
-
-#include "\z\potato\addons\core\script_macros.hpp"
