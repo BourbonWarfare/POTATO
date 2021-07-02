3781921841582024c38bb0dc4f3038c65a84ae31
PabstMirror <pabstmirror@gmail.com>
2021-07-02 11:17:39 -0500

    v1.3.11

--- addons/core/script_mod.hpp
+++ addons/core/script_mod.hpp
@@ -10,8 +10,7 @@
 
 
 // MINIMAL required version for the Mod. Components can specify others..
-#define REQUIRED_VERSION 1.94
+#define REQUIRED_VERSION 2.04
 
 // Warn if starting **NEW** mission on a BWMF version older than this
-#define EXPECTED_BWMF "2020/20/16"
-// it's 20 just to match mistake in actual
+#define EXPECTED_BWMF "2021/05/01"
