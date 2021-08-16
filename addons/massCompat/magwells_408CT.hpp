9d75af9a9d454fca7ef5ea295fcc917ffbe18cf3
Brandon Danyluk <brandondanyluk366@gmail.com>
2021-08-16 17:01:20 -0600

    fix some magazines

--- addons/massCompat/magwells_408CT.hpp
+++ addons/massCompat/magwells_408CT.hpp
@@ -1,5 +1,5 @@
     class CBA_408CT_Inter {
         GVAR(mags)[] = {
-            MAGAZINE_WELL_MAG(408,5)
+            MAGAZINE_WELL_MAG(408,7)
         };
     };           // CheyTac Intervention in .408 Chey Tac
--- addons/massCompat/magwells_762x54.hpp
+++ addons/massCompat/magwells_762x54.hpp
@@ -3,15 +3,15 @@
 
     class CBA_762x54R_LINKS {           // Links for PK, PKM, and similar
         GVAR(mags)[] = {
-            MAGAZINE_WELL_BOX(792x54,100),
-            MAGAZINE_WELL_BOX(792x54,150)
+            MAGAZINE_WELL_BOX(762x54,100),
+            MAGAZINE_WELL_BOX(762x54,150)
         };
     };
 
     class CBA_762x54R_Maxim {};         // Maxim gun in 7.62x54R
     class CBA_762x54R_Mosin {
         GVAR(mags)[] = {
-            MAGAZINE_WELL_MAG(792x54,5),
+            MAGAZINE_WELL_MAG(762x54,5),
         };
     };         // M91/30, M38, M44 Mosin
 
@@ -19,7 +19,7 @@
 
     class CBA_762x54R_SVD {             // SVD
         GVAR(mags)[] = {
-            MAGAZINE_WELL_MAG(792x54,10),
+            MAGAZINE_WELL_MAG(762x54,10),
         };
     };
 
@@ -27,6 +27,6 @@
 
     class CBA_762x54R_Vz59_LINKS {
         GVAR(mags)[] = {
-            MAGAZINE_WELL_MAG(792x54,50),
+            MAGAZINE_WELL_MAG(762x54,50),
         };
     };    //Uk vz. 59, UK59, vz 59
