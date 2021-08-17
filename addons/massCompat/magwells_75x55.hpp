112ba5d4fa6e524d8a49751d941355b44f455662
Brandon Danyluk <brandondanyluk366@gmail.com>
2021-08-16 18:24:09 -0600

    fix magwell

--- addons/massCompat/magwells_75x55.hpp
+++ addons/massCompat/magwells_75x55.hpp
@@ -1,5 +1,5 @@
     class CBA_75x55_STGW57 {
         GVAR(mags)[] = {
-            MAGAZINE_WELL_MAGS(75x55,24)
+            MAGAZINE_WELL_MAG(75x55,24)
         };
     };      // SIG SG 510-1, Stgw. 57
