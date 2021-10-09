e988242d3f13f0eacbee41750bec097f16ebe60a
Brandon Danyluk <brandondanyluk366@gmail.com>
2021-10-08 19:36:56 -0600

    normalise init speeds

--- addons/massCompat/CfgWeapons.hpp
+++ addons/massCompat/CfgWeapons.hpp
@@ -1,7 +1,3 @@
-/*
-
-*/
-
 class CfgWeapons {
     class Rifle_Base_F;
     class arifle_TRG20_F;
@@ -9,17 +5,278 @@ class CfgWeapons {
     class CUP_arifle_X95;
     class CUP_arifle_X95_Grippod;
     class CUP_arifle_AK104;
-    class CUP_CZ_BREN2_Base;
     class Pistol_Base_F;
     class Rifle_Long_Base_F;
     class Rifle_Short_Base_F;
+    class rhs_weap_ak74m;
+    class rhs_weap_ak103_base;
+    class rhs_weap_aks74;
+    class rhs_weap_svdp;
+    class hgun_PDW2000_F;
+    class CUP_arifle_DSA_SA58;
+    class CUP_arifle_G36_Base_CarryhandleOptics;
+    class CUP_arifle_G36_Base;
+    class CUP_l85a2;
+    class CUP_arifle_M4A1_BUIS_Base;
+    class CUP_saw_base;
+    class CUP_smg_p90_olive;
+    class CUP_arifle_SCAR_L_Base;
+    class CUP_arifle_Mk17_Base;
+    class CUP_arifle_ACR_EGLM_SHORT_BASE_556;
+    class CUP_arifle_ACR_EGLM_SHORT_BASE_68;
+    class CUP_arifle_ACR_Short_BASE_68;
+    class CUP_arifle_AKS_Base;
+    class CUP_CZ_BREN2_GL_Base;
+    class CUP_hgun_Makarov;
+    class CUP_arifle_XM8_Base;
 
+    // init speeds
+    class rhs_weap_ak105: rhs_weap_ak74m {
+        initSpeed = -1.0;
+    };
+    class rhs_weap_ak104: rhs_weap_ak103_base {
+        initSpeed = -1.0;
+    };
+    class rhs_weap_aks74u: rhs_weap_aks74 {
+        initSpeed = -1.0;
+    };
+    class rhs_weap_asval: rhs_weap_ak74m {
+        initSpeed = -1.0;
+    };
+    class rhs_weap_vss: rhs_weap_asval {
+        initSpeed = -1.0;
+    };
+    class rhs_weap_svds: rhs_weap_svdp {
+        initSpeed = -1.0;
+    };
+    class rhs_weap_pp2000: hgun_PDW2000_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_ACR_BASE_556: Rifle_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_ACR_Short_BASE_556: CUP_arifle_ACR_BASE_556 {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_ACR_DMR_BASE_556: CUP_arifle_ACR_BASE_556 {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_ACRC_blk_556: CUP_arifle_ACR_Short_BASE_556 {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_ACRC_EGLM_blk_556: CUP_arifle_ACR_EGLM_SHORT_BASE_556 {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_ACR_BASE_68: Rifle_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_ACR_DMR_BASE_68: CUP_arifle_ACR_BASE_68 {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_ACRC_blk_68: CUP_arifle_ACR_Short_BASE_68 {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_ACRC_EGLM_blk_68: CUP_arifle_ACR_EGLM_SHORT_BASE_68 {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_AK_Base: Rifle_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_AKS74U: CUP_arifle_AKS_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_srifle_AWM_Base: Rifle_Long_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_CZ_BREN2_Base: Rifle_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_CZ_BREN2_556_11: CUP_CZ_BREN2_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_CZ_BREN2_556_11_GL: CUP_CZ_BREN2_GL_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_CZ_BREN2_556_8: CUP_CZ_BREN2_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_hgun_Colt1911: Pistol_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_hgun_Compact: Pistol_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_CZ805_Base: Rifle_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_CZ805_A2: CUP_arifle_CZ805_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_CZ805_A1: CUP_arifle_CZ805_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_hgun_Duty: Pistol_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_smg_EVO: Rifle_Short_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_DSA_SA58_OSW: CUP_arifle_DSA_SA58 {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G3_Base: Rifle_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36A: CUP_arifle_G36_Base_CarryhandleOptics {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36A_RIS: CUP_arifle_G36_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36A3: CUP_arifle_G36_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36A3_grip: CUP_arifle_G36_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36K: CUP_arifle_G36_Base_CarryhandleOptics {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36K_VFG: CUP_arifle_G36K {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36K_RIS: CUP_arifle_G36_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36K_KSK: CUP_arifle_G36K_RIS {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36K_KSK_VFG: CUP_arifle_G36K_RIS {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36KA3: CUP_arifle_G36_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36KA3_grip: CUP_arifle_G36_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36C: CUP_arifle_G36_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36C_VFG: CUP_arifle_G36C {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36C_VFG_Carry: CUP_arifle_G36C {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36CA3: CUP_arifle_G36C {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36CA3_grip: CUP_arifle_G36C {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_G36E: CUP_arifle_G36_Base_CarryhandleOptics {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_MG36: CUP_arifle_G36C {
+        initSpeed = -1.0;
+    };
+    class CUP_hgun_Glock17: Pistol_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_HK416_145_Base: Rifle_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_HK416_Black: CUP_arifle_HK416_145_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_l85a2_base: Rifle_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_L86A2: CUP_l85a2 {
+        initSpeed = -1.0;
+    };
+    class CUP_lmg_L110A1: Rifle_Long_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_M4A1_SOMMOD_black: CUP_arifle_M4A1_BUIS_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_M4A1_SOMMOD_Grip_black: CUP_arifle_M4A1_BUIS_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_srifle_M110: Rifle_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_lmg_minimipara: CUP_saw_base {
+        initSpeed = -1.0;
+    };
+    class CUP_hgun_PMM: CUP_hgun_Makarov {
+        initSpeed = -1.0;
+    };
+    class CUP_lmg_Mk48_Base: Rifle_Long_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_smg_PS90_olive: CUP_smg_p90_olive {
+        initSpeed = -1.0;
+    };
+    class CUP_srifle_Remington700: Rifle_Long_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_Mk16_STD: CUP_arifle_SCAR_L_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_Mk16_SV: CUP_arifle_SCAR_L_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_Mk16_CQC: CUP_arifle_SCAR_L_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_Mk17_CQC: CUP_arifle_Mk17_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_Mk17_STD: CUP_arifle_Mk17_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_Mk20: CUP_arifle_Mk17_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_lmg_UK59: Rifle_Long_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_SR3M_Vikhr: Rifle_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_srifle_VSSVintorez: Rifle_Base_F {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_XM8_Railed_Base: Rifle_Base_f {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_XM8_Carbine: CUP_arifle_XM8_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_XM8_Carbine_GL: CUP_arifle_XM8_Base {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_XM8_Compact: CUP_arifle_XM8_Base {
+        initSpeed = -1.0;
+    };
+
+    // Magazine Wells
     class arifle_MSBS65_base_F: Rifle_Base_F {
         magazineWell[] += { "CBA_65x39_MX" };
     };
 
     class CUP_arifle_FNFAL: Rifle_Base_F {
         magazineWell[] += { "CBA_762x51_FAL_L", "CBA_762x51_FAL_XL" };
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_FNFAL5062: CUP_arifle_FNFAL {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_FNFAL_OSW: CUP_arifle_FNFAL {
+        initSpeed = -1.0;
     };
 
     class CUP_arifle_Fort221: arifle_TRG20_F {
@@ -50,12 +307,24 @@ class CfgWeapons {
         magazineWell[] = { "CBA_556x45_GALIL" };
     };
 
+    class CUP_arifle_Galil_black: CUP_arifle_GALIL_BASE {
+        initSpeed = -1.0;
+    };
+    class CUP_arifle_Galil_SAR_black: CUP_arifle_GALIL_BASE {
+        initSpeed = -1.0;
+    };
+
     class CUP_arifle_SAIGA_MK03: CUP_arifle_AK104 {
         magazineWell[] += { QGVAR(magwell_545x39) };
     };
 
     class CUP_CZ_BREN2_762_Base: CUP_CZ_BREN2_Base {
         magazineWell[] += { "CBA_762x51_AR10", "CBA_762x51_AR10_L", "CBA_762x51_AR10_XL" };
+        initSpeed = -1.0;
+    };
+
+    class CUP_CZ_BREN2_762_8: CUP_CZ_BREN2_762_Base {
+        initSpeed = -1.0;
     };
 
     class CUP_Famas_Base: Rifle_Base_F {
@@ -72,6 +341,7 @@ class CfgWeapons {
 
     class CUP_hgun_M17_Coyote: Pistol_Base_F {
         magazineWell[] += { QGVAR(magwell_9x19_m17) };
+        initSpeed = -1.0;
     };
 
     class CUP_Mac10_Base: Rifle_Base_F {
@@ -84,10 +354,12 @@ class CfgWeapons {
 
     class CUP_hgun_Mk23: Pistol_Base_F {
         magazineWell[] += { QGVAR(magwell_45acp_mk23) };
+        initSpeed = -1.0;
     };
 
     class CUP_hgun_TaurusTracker455: Pistol_Base_F {
         magazineWell[] += { "Cylinder_45ACP" };
+        initSpeed = -1.0;
     };
 
     class CUP_hgun_TEC9: Pistol_Base_F {
@@ -136,5 +408,9 @@ class CfgWeapons {
 
     class CUP_arifle_OTS14_GROZA_Base: Rifle_Base_F {
         magazineWell[] += { "CBA_9x39_VSS" };
+    };
+
+    class CUP_arifle_OTS14_GROZA_762_base: CUP_arifle_OTS14_GROZA_Base {
+        initSpeed = -1.0;
     };
 };
