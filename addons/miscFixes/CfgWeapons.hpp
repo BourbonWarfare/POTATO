4a14ad25eb5166001f8a2d5a658a75bcc65e9a46
Alablm <aidan197@hotmail.com>
2021-10-11 12:14:59 -0400

    Alice Webbing Fix
    Directs plot armor alice webbing to the correct CUP model

--- addons/miscFixes/CfgWeapons.hpp
+++ addons/miscFixes/CfgWeapons.hpp
@@ -74,10 +74,10 @@ class CfgWeapons {
     // Add "plot armor" to some vests /* From V_PlateCarrier2_rgr */
 
     class ItemCore;
-    class CUP_Vest_Camo_Base: ItemCore {
+    class CUP_V_B_Alice: ItemCore {
         class ItemInfo;
     };
-    class rhsgref_alice_webbing_bwArmorMod: CUP_Vest_Camo_Base { // must keep old classname, but this is now based on CUP
+    class rhsgref_alice_webbing_bwArmorMod: CUP_V_B_Alice { // must keep old classname, but this is now based on CUP
         displayName = "ALICE Webbing [coop plot armor]";
         descriptionShort = "Armor Level IV";
         class ItemInfo: ItemInfo {
