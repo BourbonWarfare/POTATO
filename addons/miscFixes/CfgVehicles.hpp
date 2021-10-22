de8c2d8c514a05def420bf1f5bab9763b41d1c9d
PabstMirror <pabstmirror@gmail.com>
2021-10-21 20:20:44 -0500

    Fix FFAA XEH (#332)

--- addons/miscFixes/CfgVehicles.hpp
+++ addons/miscFixes/CfgVehicles.hpp
@@ -76,6 +76,20 @@ class CfgVehicles {
     class DSA_DeltaX_Operator: B_Soldier_F { class EventHandlers { class CBA_Extended_EventHandlers: CBA_Extended_EventHandlers {}; }; }; // Drongos Spooks and Anomalies> Delta X Operator
     class DSA_DeltaX_CBRN: B_Soldier_F { class EventHandlers { class CBA_Extended_EventHandlers: CBA_Extended_EventHandlers {}; }; }; // Drongos Spooks and Anomalies> Delta X CBRN
 
+    // Fix: FFAA
+    class FFAA_rapel_logica: Logic { SLX_XEH_DISABLED = 1; }; // @FFAA -> FFAA RAPEL
+    class Air;
+    class FFAA_rapel: Air { SLX_XEH_DISABLED = 1; }; // @FFAA -> Rappel rope
+    class StaticWeapon;
+    class FFAA_rapelf: StaticWeapon { SLX_XEH_DISABLED = 1; }; // @FFAA -> Rappel rope
+    class PlaneWreck;
+    class ffaa_ea_hercules_wreck: PlaneWreck { SLX_XEH_DISABLED = 1; }; // @FFAA -> Wreck
+    class ffaa_AV8BWreck: PlaneWreck { SLX_XEH_DISABLED = 1; }; // @FFAA -> Wreck
+    class ffaa_mq9_reaper_Wreck: PlaneWreck { SLX_XEH_DISABLED = 1; }; // @FFAA -> Wreck
+    class HelicopterWreck;
+    class ffaa_et_ch47Wreck: HelicopterWreck { SLX_XEH_DISABLED = 1; }; // @FFAA -> Wreck
+    class ffaa_famet_cougarWreck: HelicopterWreck { SLX_XEH_DISABLED = 1; }; // @FFAA -> Wreck
+
     // Fix fajita
 	class Building;
 	class NonStrategic: Building {
