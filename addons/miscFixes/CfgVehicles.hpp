e8087685b3a6cbbaf0347c5e07c4c3807c5c3f7f
kilo <alex.b.gust@gmail.com>
2022-10-17 12:57:08 -0700

    add credits for finding BIS method used

--- addons/miscFixes/CfgVehicles.hpp
+++ addons/miscFixes/CfgVehicles.hpp
@@ -50,7 +50,7 @@ class CfgVehicles {
         class TransportBackpacks {};
     };
 
-    // Make vanilla Chinook and EH302 (IDAP/FIA transport heli) float on water. Matches CUP Chinook/CH-53e and other heavy lift helos. Should be extended to all helicopters from mods that could feasibly float. 
+    // Make vanilla Chinook and EH302 (IDAP/FIA transport heli) float on water. Matches CUP Chinook/CH-53e and other heavy lift helos. Should be extended to all helicopters from mods that could feasibly float. Direction on BIS method from Steam Workshop user "crub"
     class Helicopter_Base_H;
 	class Heli_Transport_03_base_F: Helicopter_Base_H {waterLeakiness=0.1;};
     class Heli_Transport_02_base_F: Helicopter_Base_H {waterLeakiness=0.1;};
