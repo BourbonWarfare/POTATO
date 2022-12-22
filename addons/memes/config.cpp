d9b16375203fa9c804da3b2e5815fd6ab919b55f
PabstMirror <pabstmirror@gmail.com>
2022-12-22 17:00:22 -0600

    fix path changes

--- addons/memes/config.cpp
+++ addons/memes/config.cpp
@@ -3,43 +3,15 @@
 class CfgPatches {
     class ADDON {
         units[] = {"potato_cheezItBoxPlaceable", "potato_serverPlaceable", "potato_gamestopPlaceable"};
-        weapons[] = {"potato_cheezItBox", "potato_serverBox"};
+        weapons[] = {"potato_cheezItBox", "potato_serverBox", "potato_funGun_red", "potato_funGun_green"};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"potato_core"};
         author = "Potato";
         authorUrl = "https://github.com/BourbonWarfare/POTATO";
         VERSION_CONFIG;
     };
-    class Pabst_funGun {
-        units[]={};
-        weapons[]={"potato_funGun_red", "potato_funGun_green"};
-        requiredVersion=0.1;
-        requiredAddons[]={"A3_Weapons_F", "A3_Weapons_F_Exp_SMGs_SMG_05"};
-    };
-};
-
-class CfgWeapons {
-    class SMG_05_F;
-
-    class potato_funGun_red: SMG_05_F {
-        author = "Pabst";
-        scope = 1; // Keep hidden
-        scopeCurator = 0;
-        displayName = "Candy Cane 9mm (Red)";
-        model = "\z\potato\addons\memes\data\funGun_red.p3d";
-        selectionFireAnim = "muzzleFlash";
-        handAnim[] = {"OFP2_ManSkeleton","\A3\Weapons_F\Rifles\MX\data\Anim\mx.rtm"};
-    };
-    class potato_funGun_green: SMG_05_F {
-        author = "Pabst";
-        scope = 1; // Keep hidden
-        scopeCurator = 0;
-        displayName = "Candy Cane 9mm (Green)";
-        model = "\z\potato\addons\memes\data\funGun_green.p3d";
-        selectionFireAnim = "muzzleFlash";
-        handAnim[] = {"OFP2_ManSkeleton","\A3\Weapons_F\Rifles\MX\data\Anim\mx.rtm"};
-    };
 };
 
 #include "CfgVehicles.hpp"
+#include "CfgWeapons.hpp"
 #include "CfgLeaflets.hpp"
