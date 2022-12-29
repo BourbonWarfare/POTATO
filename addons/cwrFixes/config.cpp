cdd020579aa9464d5fd25fb518b3426295acb3a2
PabstMirror <pabstmirror@gmail.com>
2022-12-29 13:45:46 -0600

    cwr - fix zeus popup (#385)

--- addons/cwrFixes/config.cpp
+++ addons/cwrFixes/config.cpp
@@ -8,7 +8,7 @@ class CfgPatches {
         weapons[] = {};
         magazines[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"potato_core", "cwr3_intro", "cwr3_vehicle_m41"};
+        requiredAddons[] = {"potato_core", "cwr3_intro", "cwr3_vehicle_m41", "cwr3_soldiers_us"};
         author = "Potato";
         authorUrl = "https://github.com/BourbonWarfare/POTATO";
         VERSION_CONFIG;
@@ -17,5 +17,23 @@ class CfgPatches {
 
 #include "CfgEventHandlers.hpp"
 #include "CfgVehicles.hpp"
+
+class CfgGroups {
+	class West {
+        class CWR3_USA {
+            class Infantry80 {
+                class cwr3_b_rifle_squad80 {
+                    class Unit4 { vehicle = "cwr3_b_soldier80_ar_m16"; };
+                };
+                class cwr3_b_fire_team80 {
+                    class Unit1 { vehicle = "cwr3_b_soldier80_ar_m16"; };
+                };
+                class cwr3_b_support_team80 {
+                    class Unit1 { vehicle = "cwr3_b_soldier80_ar_m16"; };
+                };
+            };
+        };
+    };
+};
 
 #endif
