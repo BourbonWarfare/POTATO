7f655a00b5f5961f302fe90158085ed9899032e5
PabstMirror <pabstmirror@gmail.com>
2023-02-04 13:15:05 -0600

    tweak aiCfgFix macros to match code gen

--- addons/aiCfgFixes/script_macros.hpp
+++ addons/aiCfgFixes/script_macros.hpp
@@ -3,10 +3,15 @@
 #define CFG_FOOTER "};" + LINE_BREAK + LINE_BREAK
 #define CFG_CLASS_DATA(dataName, data) INDENT + INDENT + dataName + " = " + data + ";" + LINE_BREAK
 
-#define AI_AUDIBLE_FIRE_0 100;  // regular ammo
-#define AI_AUDIBLE_FIRE_1 10;   // subsonic ammo
-#define AI_AUDIBLE_FIRE_2 200;  // vehicle ammo (30mm autocannon, hellfire, etc)
-#define AI_AUDIBLE_FIRE_3 50;   // Grenade Launchers (40mm UGL, VOG25, etc)
+
+// regular ammo
+#define AI_AUDIBLE_FIRE_0 100
+// subsonic ammo
+#define AI_AUDIBLE_FIRE_1 10
+// vehicle ammo (30mm autocannon, hellfire, etc)
+#define AI_AUDIBLE_FIRE_2 200
+// Grenade Launchers (40mm UGL, VOG25, etc)
+#define AI_AUDIBLE_FIRE_3 50
 
 /*
 audibleFire = AI_AUDIBLE_FIRE_0;
