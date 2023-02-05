2b91268480ed9eabedf8fac6009c2740bb93ff96
PabstMirror <pabstmirror@gmail.com>
2023-02-05 16:49:13 -0600

    csw/mg now potato addons

--- addons/missileGuidanceCompat/config.cpp
+++ addons/missileGuidanceCompat/config.cpp
@@ -9,8 +9,7 @@
 #endif
 
 #ifdef PATCH_SKIP
-// Use double (potato_ prefix) so ace doesn't complain about outdated pbo
-POTATO_PATCH_NOT_LOADED(DOUBLES(POTATO,ADDON),PATCH_SKIP)
+POTATO_PATCH_NOT_LOADED(ADDON,PATCH_SKIP)
 #else
 class CfgPatches {
     class ADDON {
