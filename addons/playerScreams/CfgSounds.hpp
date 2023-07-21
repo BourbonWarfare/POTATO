ffc6f9f7f302e532fbfa9dc34d5877b8f446f837
PabstMirror <pabstmirror@gmail.com>
2023-07-20 20:52:10 -0500

    use gvar prefix

--- addons/playerScreams/CfgSounds.hpp
+++ addons/playerScreams/CfgSounds.hpp
@@ -1,51 +1,51 @@
 class CfgSounds {
-    class hit_snd_01 {
-        name = "hit_snd_01";
+    class GVAR(hit_01) {
+        name = QGVAR(hit_01);
         sound[] = {"A3\Sounds_F\characters\human-sfx\P01\Max_Hit_01.wss", 1.25, 1, 200};
         titles[] = {};
     };
-    class hit_snd_02 {
-        name = "hit_snd_02";
+    class GVAR(hit_02) {
+        name = QGVAR(hit_02);
         sound[] = {"A3\Sounds_F\characters\human-sfx\P02\Mid_Hit_01.wss", 1.25, 1, 200};
         titles[] = {};
     };
-    class hit_snd_03 {
-        name = "hit_snd_03";
+    class GVAR(hit_03) {
+        name = QGVAR(hit_03);
         sound[] = {"A3\Sounds_F\characters\human-sfx\P03\Hit_Mid_1.wss", 1.25, 1, 200};
         titles[] = {};
-    };
-    class hit_snd_04 {
-        name = "hit_snd_04";
+    }; 
+    class GVAR(hit_04) {
+        name = QGVAR(hit_04);
         sound[] = {"A3\Sounds_F\characters\human-sfx\P03\Hit_Mid_3.wss", 1.25, 1, 200};
         titles[] = {};
     };
-    class hit_snd_05 {
-        name = "hit_snd_05";
+    class GVAR(hit_05) {
+        name = QGVAR(hit_05);
         sound[] = {"A3\Sounds_F\characters\human-sfx\P04\Hit_Max_3.wss", 1.25, 1, 200};
         titles[] = {};
     };
-    class hit_snd_06 {
-        name = "hit_snd_06";
+    class GVAR(hit_06) {
+        name = QGVAR(hit_06);
         sound[] = {"A3\Sounds_F\characters\human-sfx\P05\Hit_Max_2.wss", 1.25, 1, 200};
         titles[] = {};
     };
-    class hit_snd_07 {
-        name = "hit_snd_07";
+    class GVAR(hit_07) {
+        name = QGVAR(hit_07);
         sound[] = {"A3\Sounds_F\characters\human-sfx\P05\Hit_Max_4.wss", 1.25, 1, 200};
         titles[] = {};
     };
-    class hit_snd_08 {
-        name = "hit_snd_08";
+    class GVAR(hit_08) {
+        name = QGVAR(hit_08);
         sound[] = {"A3\Sounds_F\characters\human-sfx\P04\Hit_Mid_3.wss", 1.25, 1, 200};
         titles[] = {};
     };
-    class hit_snd_09 {
-        name = "hit_snd_09";
+    class GVAR(hit_09) {
+        name = QGVAR(hit_09);
         sound[] = {"A3\Sounds_F\characters\human-sfx\P08\Hit_Max_4.wss", 1.25, 1, 200};
         titles[] = {};
     };
-    class hit_snd_10 {
-        name = "hit_snd_10";
+    class GVAR(hit_10) {
+        name = QGVAR(hit_10);
         sound[] = {"A3\Sounds_F\characters\human-sfx\P09\Hit_Max_3.wss", 1.25, 1, 200};
         titles[] = {};
     };
--- addons/playerScreams/client_postinit.sqf
+++ addons/playerScreams/client_postinit.sqf
@@ -1,8 +1,12 @@
-potato_player_screams_var_soundList = ["hit_snd_01","hit_snd_02","hit_snd_03","hit_snd_04","hit_snd_05","hit_snd_06","hit_snd_07","hit_snd_08","hit_snd_09","hit_snd_10"];
+#include "script_component.hpp"
+
+if (!hasInterface) exitWith {};
+
+GVAR(soundList) = [QGVAR(hit_01),QGVAR(hit_02),QGVAR(hit_03),QGVAR(hit_04),QGVAR(hit_05),QGVAR(hit_06),QGVAR(hit_07),QGVAR(hit_08),QGVAR(hit_09),QGVAR(hit_10)];
 
 ["ace_unconscious", {
     params ["_unit", "_state"];
-
     if (!_state || { !alive _unit } || { !isNull objectParent _unit }) exitWith {};
-    _unit say3D [(selectRandom potato_player_screams_var_soundList), 200, 1, false];
+    if (!(missionNamespace getVariable [QGVAR(enabled), true])) exitWith {};
+    _unit say3D [(selectRandom GVAR(soundList)), 200, 1, false];
 }] call CBA_fnc_addEventHandler;
