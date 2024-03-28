80d5a1d7d93a3d0f95ce41a1c05f98588ce282a3
PabstMirror <pabstmirror@gmail.com>
2024-03-27 23:03:01 -0500

    PatchPTV - Fix XEH configs (#478)
    * PatchPTV - Fix XEH configs
    
    * Update config.cpp
    
    * Update config.cpp

--- addons/miscFixes/patchPTV/config.cpp
+++ addons/miscFixes/patchPTV/config.cpp
@@ -7,7 +7,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = { "potato_core", "ptv_characters_cfg", "ptv_characters", "ptv_sounds", "jsrs_soundmod_complete_edition", "jsrs_soundmod_complete_edition_soundfiles" };
+        requiredAddons[] = { "potato_core", "ptv_vehicles", "ptv_characters_cfg", "ptv_characters", "ptv_sounds", "jsrs_soundmod_complete_edition", "jsrs_soundmod_complete_edition_soundfiles" };
         skipWhenMissingDependencies = 1;
         author = "Bourbon Warfare";
         authorUrl = "https://github.com/BourbonWarfare/POTATO";
@@ -15,10 +15,32 @@ class CfgPatches {
     };
 };
 
+
+
+class CBA_Extended_EventHandlers;
+class CfgVehicles {
+    // Fix XEH: "[CBA] (xeh) WARNING: ptv_Pnd_Base_F does not support Extended Event Handlers! Addon: 3092130870"
+    class B_APC_Wheeled_03_cannon_F;
+    class ptv_Pnd_Base_F: B_APC_Wheeled_03_cannon_F {
+        class EventHandlers { class CBA_Extended_EventHandlers: CBA_Extended_EventHandlers {}; };
+    };
+    class I_APC_tracked_03_cannon_F;
+    class ptv_Warrior_Base_F: I_APC_tracked_03_cannon_F {
+        class EventHandlers { class CBA_Extended_EventHandlers: CBA_Extended_EventHandlers {}; };
+    };
+
+
+    // Increase storage capacity of Contact DLC Radio Backpack to hold ACRE radios and radio accessories.
+    class B_RadioBag_01_base_F;
+    class ptv_RadioBag_01_Desert: B_RadioBag_01_base_F {maximumLoad=300;}; // from Project True Viking
+    class ptv_RadioBag_01_Woodland: B_RadioBag_01_base_F {maximumLoad=300;};
+};
+
+
 class Mode_SemiAuto;
 class Mode_Burst;
 class Mode_FullAuto;
-class cfgWeapons {
+class CfgWeapons {
     class Pistol;
     class Pistol_Base_F: Pistol {};
     class Rifle;
