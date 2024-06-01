29072863bd483b27d11bd2067728afc6e76b3475
AChesheireCat <paraglader@gmail.com>
2024-06-01 02:42:52 -0400

    Adds Subtle Audio SFX for Medical Actions
    Stolen shamelessly from the ARK_Inhourse git repo, all this does is add some subtle and immersive SFX for the ACE diagnose action, tourniquet action, and splinting action.

---
+++ addons/miscMedical/ACE_Medical_Treatment_Actions.hpp
@@ -0,0 +1,11 @@
+class ace_medical_treatment_actions {
+    class ApplyTourniquet: BasicBandage {
+        sounds[] = {{"x\POTATO\addons\miscMedical\data\tourniquet.ogg",5,1,50}};
+    };
+    class Splint: BasicBandage {
+        sounds[] = {{"x\POTATO\addons\miscMedical\data\splint.ogg",5,1,50}};
+    };
+    class Diagnose: BasicBandage {
+        sounds[] = {{"x\POTATO\addons\miscMedical\data\diag.ogg",5,1,50}};
+    };
+};
--- addons/miscMedical/config.cpp
+++ addons/miscMedical/config.cpp
@@ -14,3 +14,4 @@ class CfgPatches {
 
 #include "CfgEventHandlers.hpp"
 #include "Naloxone_Patch.hpp"
+#include "ACE_Medical_Treatment_Actions.hpp"
