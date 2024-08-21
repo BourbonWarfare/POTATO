243c9030ece8b810313d28a8e34fbb4cfd294541
lambdatiger <spam.stnuocca.sl@gmail.com>
2024-08-20 23:38:28 -0500

    Can't find real parameters, but this mimics what I've seen in video pretty well.

--- addons/missileGuidanceCompat/CfgMissileMilan.hpp
+++ addons/missileGuidanceCompat/CfgMissileMilan.hpp
@@ -1,12 +1,12 @@
 class ace_missileguidance {
     enabled = 1;
 
-    pitchRate = 20;          // Minium flap deflection for guidance
-    yawRate = 20;            // Maximum flap deflection for guidance
+    pitchRate = 60;          // Minium flap deflection for guidance
+    yawRate = 60;            // Maximum flap deflection for guidance
 
     canVanillaLock = 0;          // Can this default vanilla lock? Only applicable to non-cadet mode
 
-    showTrail = 1;
+    showTrail = 0;
 
     // Guidance type for munitions
     defaultSeekerType = "SACLOS";
@@ -18,14 +18,14 @@ class ace_missileguidance {
     defaultNavigationType = "Line";
     navigationTypes[] = { "Line" };
 
-    lineGainP = 4;
-    lineGainD = 2;
+    lineGainP = 25;
+    lineGainD = 12;
 
-    initialPitch = 0;
+    initialPitch = -0.4;
 
     seekLastTargetPos = 0;      // seek last target position [if seeker loses LOS of target, continue to last known pos]
-    seekerAngle = 30;           // Angle from the shooter's view that can track the missile
-    seekerAccuracy = 1;         // seeker accuracy multiplier
+    seekerAngle = 30;           // Angle from the shooter's view that can track the missile, implemented
+    seekerAccuracy = 1;         // seeker accuracy multiplier, not implemented?
 
     seekerMinRange = 100;
     seekerMaxRange = 2000;      // Range from the missile which the seeker can visually search
