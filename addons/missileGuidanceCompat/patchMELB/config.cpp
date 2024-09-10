d7912f4ec31db346918ab16740b64a07a3edbde7
Bailey Danyluk <baileydanyluk@gmail.com>
2024-09-09 21:47:38 -0600

    update MELB

--- addons/missileGuidanceCompat/patchMELB/config.cpp
+++ addons/missileGuidanceCompat/patchMELB/config.cpp
@@ -17,53 +17,13 @@ class CfgPatches {
 
 
 class CfgAmmo {
+    class ACEGVAR(missileguidance,type_Hellfire);
     class Missile_AGM_02_F;
     class MELB_AGM_114K: Missile_AGM_02_F {
         irLock = 0;
         laserLock = 0;
         manualControl = 0;
 
-        class ace_missileguidance {
-            enabled = 1;
-
-            pitchRate = 30; // degrees per second
-            yawRate = 30;
-
-            canVanillaLock = 0;          // Can this default vanilla lock? Only applicable to non-cadet mode
-
-            // Guidance type for munitions
-            defaultSeekerType = "SALH";
-            seekerTypes[] = { "SALH" };
-
-            defaultSeekerLockMode = "LOAL";
-            seekerLockModes[] = { "LOAL", "LOBL" };
-
-            defaultNavigationType = "Direct";
-            navigationTypes[] = { "Direct", "ZeroEffortMiss" };
-
-            seekLastTargetPos = 1;      // seek last target position [if seeker loses LOS of target, continue to last known pos]
-            seekerAngle = 70;           // Angle in front of the missile which can be searched
-            seekerAccuracy = 1;         // seeker accuracy multiplier
-
-            seekerMinRange = 1;
-            seekerMaxRange = 8000;      // Range from the missile which the seeker can visually search
-
-            // Attack profile type selection
-            defaultAttackProfile = "hellfire";
-            attackProfiles[] = {"hellfire", "hellfire_hi", "hellfire_lo"};
-
-            class navigationStates {
-                class initial {
-                    transitionCondition = "ace_hellfire_fnc_midCourseTransition";
-                    navigationType = "Direct";
-                };
-                class terminal {
-                    transitionCondition = "";
-                    navigationType = "ZeroEffortMiss";
-                };
-                // transitions from initial -> termimal
-                states[] = {"initial", "terminal"};
-            };
-        };
+        ACE_MISSILE(Hellfire);
     };
 };
