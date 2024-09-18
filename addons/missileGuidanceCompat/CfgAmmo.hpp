5920cadce364c96a012cdd9ae5f415dcebd74b29
PabstMirror <pabstmirror@gmail.com>
2024-09-17 22:27:29 -0500

    cleanup unused redeye config

--- addons/missileGuidanceCompat/CfgAmmo.hpp
+++ addons/missileGuidanceCompat/CfgAmmo.hpp
@@ -1,13 +1,6 @@
 class CfgAmmo {
-    class CUP_M_Stinger_AA;
     class ShellBase;
-
     class ace_dragon_serviceCharge: ShellBase {
-        soundSetExplosion[] = {};
-    };
-    class GVAR(redeye): CUP_M_Stinger_AA {
-        maneuvrability = 0;
-        ACE_MISSILE(Redeye);
+        soundSetExplosion[] = {}; // prevent jsrs from modifying this
     };
 };
-
