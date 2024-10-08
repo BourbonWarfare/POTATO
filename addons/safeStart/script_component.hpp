3d14d286430d4fb12e0b1cab3419a30cd58d4d94
lambdatiger <spam.stnuocca.sl@gmail.com>
2024-10-08 16:27:14 -0500

    Turning debug mode off

--- addons/safeStart/script_component.hpp
+++ addons/safeStart/script_component.hpp
@@ -1,7 +1,7 @@
 #define COMPONENT safestart
 #include "\z\potato\addons\core\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
+// #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
