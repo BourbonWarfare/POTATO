8d54d0f56edef1a9ac6ba88de1ac36bd8d0db536
AChesheireCat <paraglader@gmail.com>
2024-10-21 18:43:57 -0400

    Breaks out ace_medical_treatment
    Moves the ace_medical_treatment section to its own file "ACE_Medical_Treatment.hpp"
    
    Added new file to includes section under config.cpp

--- addons/miscMedical/config.cpp
+++ addons/miscMedical/config.cpp
@@ -12,28 +12,9 @@ class CfgPatches {
     };
 };
 
-class ace_medical_treatment {
-    class Medication {
-        class Naloxone {
-            painReduce = 0;
-            hrIncreaseLow[] = {0, 0};
-            hrIncreaseNormal[] = {0, 0};
-            hrIncreaseHigh[] = {0, 0};
-            timeInSystem = 180;
-            timeTillMaxEffect = 1;
-            maxDose = 0.1;
-            onOverDose = QFUNC(Naloxone);
-            incompatibleMedication[] = {};
-            viscosityChange = 0;
-        };
-        class Painkillers;
-        class Pkblister: Painkillers {};
-    };
-};
-
-
 #include "CfgEventHandlers.hpp"
 #include "CfgMagazines.hpp"
 #include "CfgVehicles.hpp"
 #include "CfgWeapons.hpp"
 #include "ACE_Medical_Treatment_Actions.hpp"
+#include "ACE_Medical_Treatment.hpp"
