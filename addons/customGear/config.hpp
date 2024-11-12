b37096e39e78c19c65dd0cb31a48d1cbed7226bc
AChesheireCat <paraglader@gmail.com>
2024-11-12 17:29:05 -0500

    Adds weapons to config.cpp

--- addons/customGear/config.hpp
+++ addons/customGear/config.hpp
@@ -3,7 +3,7 @@
 class CfgPatches {
     class ADDON {
         units[] = {};
-        weapons[] = {};
+        weapons[] = { "potato_fauxC7_c7", "potato_fauxC7_c7GL", "potato_fauxC7_c7a2", "potato_fauxC7_c7a2GL", "potato_fauxMG5_MG5" };
         magazines[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = { "potato_core" };
