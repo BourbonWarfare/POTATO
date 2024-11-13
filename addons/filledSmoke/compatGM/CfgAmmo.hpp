e175ffdc1e4da9aacc08368130c517920d0d134d
lambdatiger <spam.stnuocca.sl@gmail.com>
2024-11-13 16:45:51 -0600

    Add GM smoke compat

---
+++ addons/filledSmoke/compatGM/CfgAmmo.hpp
@@ -0,0 +1,9 @@
+class CfgAmmo {
+    class gm_SmokeShell_wht;
+    class gm_smokeshell_wht_dm25: gm_SmokeShell_wht {
+        effectsSmoke = QEGVAR(filledSmoke,bigSmoke_GM);
+    };
+    class gm_smokeshell_wht_gc: gm_SmokeShell_wht {
+        effectsSmoke = QEGVAR(filledSmoke,bigSmoke_GM);
+    };
+};
