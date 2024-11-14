c366c90adc0e3467c7ad271e8ebd98b08d1b2d8c
AChesheireCat <paraglader@gmail.com>
2024-11-14 03:05:40 -0500

    Kimi's HMD 20mm Cannon Fix
    Kimi's HMD mod broke the inheritance for the "gatling_20mm" cannon, breaking it on any dynamic pylon vehicle.
    
    I also updated the cursorAim, ballisticsComputer, and magazines to be in-line with other, similar weapons, and to include ACE ammunition.

---
+++ addons/miscFixes/patchHMD/config.cpp
@@ -0,0 +1,29 @@
+#include "\z\potato\addons\miscFixes\script_component.hpp"
+#undef COMPONENT
+#define COMPONENT miscFixes_patchHMD
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = { "Kimi_Weapons_Helos" };
+        skipWhenMissingDependencies = 1;
+        author = "Bourbon Warfare";
+        authorUrl = "https://github.com/BourbonWarfare/POTATO";
+        VERSION_CONFIG;
+    };
+};
+
+class Mode_SemiAuto;
+class Mode_Burst;
+class Mode_FullAuto;
+class cfgWeapons {
+    class Default;
+    class CannonCore: Default {};
+    class gatling_20mm: CannonCore {
+        cursorAim = "mg";
+        ballisticsComputer = "1 + 2 + 16";
+        magazines[] = {"2000Rnd_20mm_shells","1000Rnd_20mm_shells","300Rnd_20mm_shells","PylonWeapon_300Rnd_20mm_shells","ACE_500Rnd_20mm_shells_Comanche"};
+    };
+};
