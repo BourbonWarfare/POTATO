6f4b274d4bb2b8480360957c17712b0c4fcf802e
AChesheireCat <66699749+AChesheireCat@users.noreply.github.com>
2024-11-16 20:16:46 -0500

    Adds a low-count painkiller alternative (#583)
    * Adds a low-count painkiller alternative
    
    This is a first pass try - it is very much a WIP. I still need to test it.
    
    https://github.com/BourbonWarfare/POTATO/issues/582
    
    and
    
    https://github.com/BourbonWarfare/bwmf/pull/203#issuecomment-2427566788
    
    This is the aforementioned "blister pack" of painkillers.
    
    - Only has 2 uses compared to the 10 that the bottle has
    
    - Intent is to be issued in the basic kit so supplement medic capacity to treat pain
    
    * Forgot to update config.cpp
    
    Added Painkiller_Patch.hpp to #include order.
    
    * Requested Fixes
    
    - Split original Painkiller_Patch.hpp into their respective files (CfgMagazines.hpp, CfgVehicles.hpp, CfgWeapons.hpp)
    
    - Fixed image pathing per HEMTT
    
    - Removed redundant line in ACE_Medical_Treatment_Actions.hpp
    
    - Updated config.cpp to include the new broken out files
    
    * Broke-out Naloxone_Patch.hpp
    
    Moved the CfgVehicles and CfgWeapons sections from Naloxone_Patch.hpp to the new broken-out files CfgVehicles.hpp and CfgWeapons.hpp
    
    * Fixed multiple defines
    
    I'm dumb... Fixed the duplicate CfgVehicles and CfgWeapons.
    
    * Moved ace_medical_treatment to config.cpp
    
    Moved the ace_medical_treatment sections from their respective files (Naloxone_Patch.hpp, Painkiller_Patch.hpp) to config.cpp to consolidate and remove multiple defines.
    
    Can rebase elsewhere if this solution is a no-go!
    
    * Breaks out ace_medical_treatment
    
    Moves the ace_medical_treatment section to its own file "ACE_Medical_Treatment.hpp"
    
    Added new file to includes section under config.cpp
    
    * Properly adds pkblister item
    
    Thanks Pabst <3
    
    Co-authored-by: PabstMirror <pabstmirror@gmail.com>
    
    * add to items
    
    ---------
    
    Co-authored-by: PabstMirror <pabstmirror@gmail.com>

---
+++ addons/miscMedical/CfgWeapons.hpp
@@ -0,0 +1,12 @@
+// License Attribution req: UI image authored by user grmarcstock via Vecteezy.com
+
+class CfgWeapons {
+    class ACE_atropine;
+    class potato_naloxone: ACE_atropine {
+        scope = 2;
+        author = "Alablm";
+        displayName = "Naloxone Autoinjector";
+        descriptionShort = "Used to treat opioid overdose";
+        descriptionUse = "A drug widely used to treat opioid overdoses by blocking opioid receptors";
+    };
+};
