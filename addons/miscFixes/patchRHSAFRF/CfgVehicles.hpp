bca93eb859eced33d44012ee66a56ebc3271b0a9
lambdatiger <spam.stnuocca.sl@gmail.com>
2024-11-16 19:15:19 -0600

    Revert RHS Change to ammo_Penetrator_Base (#587)
    * formatted addon for easier editing
    
    * Updated sbase spalling class edited by RHS

---
+++ addons/miscFixes/patchRHSAFRF/CfgVehicles.hpp
@@ -0,0 +1,9 @@
+class CfgVehicles {
+    class B_AssaultPack_Base;
+    class rhs_medic_bag: B_AssaultPack_Base {};
+    class rhs_medic_bag_XL: rhs_medic_bag {
+        author = "RHS & AChesheireCat";
+        displayName = "Medic Bag (Large)";
+        maximumLoad = 240;
+    };
+};
