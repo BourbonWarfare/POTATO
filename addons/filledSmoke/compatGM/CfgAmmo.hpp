6976a5d3558d6852925af86b8acd2751c784b156
PabstMirror <pabstmirror@gmail.com>
2024-11-20 19:32:07 -0600

    Cleanup some base classes (#606)

--- addons/filledSmoke/compatGM/CfgAmmo.hpp
+++ addons/filledSmoke/compatGM/CfgAmmo.hpp
@@ -1,9 +1,9 @@
 class CfgAmmo {
-    class gm_SmokeShell_wht;
-    class gm_smokeshell_wht_dm25: gm_SmokeShell_wht {
+    class gm_SmokeShell_Base;
+    class gm_smokeshell_wht_dm25: gm_SmokeShell_Base {
         effectsSmoke = QEGVAR(filledSmoke,bigSmoke_GM);
     };
-    class gm_smokeshell_wht_gc: gm_SmokeShell_wht {
+    class gm_smokeshell_wht_gc: gm_SmokeShell_Base {
         effectsSmoke = QEGVAR(filledSmoke,bigSmoke_GM);
     };
 };
