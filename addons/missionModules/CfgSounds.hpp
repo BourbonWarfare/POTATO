293189a6b39d7be7a505a897832eca460e2b9b0d
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-02-10 17:56:39 -0600

    Fxied audio

--- addons/missionModules/CfgSounds.hpp
+++ addons/missionModules/CfgSounds.hpp
@@ -1,10 +1,10 @@
 class CfgSounds {
     class GVAR(vanillaDefaultNotification) {
-        sound[] = {"@a3\Ui_f\data\Sound\CfgNotifications\defaultNotification", 1.5, 1};
+        sound[] = {"\a3\Ui_f\data\Sound\CfgNotifications\defaultNotification", 1.5, 1};
         titles[] = {};
     };
     class GVAR(vanillaDefaultNotificationClose) {
-        sound[] = {"@a3\Ui_f\data\Sound\CfgNotifications\defaultNotificationClose", 1, 1};
+        sound[] = {"\a3\Ui_f\data\Sound\CfgNotifications\defaultNotificationClose", 1, 1};
         titles[] = {};
     };
 };
