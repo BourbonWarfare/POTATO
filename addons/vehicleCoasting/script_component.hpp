54fdce35fb5aa680b27ad7513347633013a17195
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-02-23 00:06:56 -0600

    extended loop time

--- addons/vehicleCoasting/script_component.hpp
+++ addons/vehicleCoasting/script_component.hpp
@@ -7,11 +7,11 @@
 // #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
-#define COAST_LOOP_INTERVAL 0.2
+#define COAST_LOOP_INTERVAL 0.5
 // 50% chance for cook-off
 #define POTATO_VEHICLEC_COASTING_COOKOFF_CHANCE 0.5
 // 50% chance with just driver dying
 #define POTATO_VEHICLEC_COASTING_DRIVER_CHANCE 0.5
-// Combined chance of 75% if both happen
+// Combined chance of 75% if both happen to both
 
 #include "\z\potato\addons\core\script_macros.hpp"
