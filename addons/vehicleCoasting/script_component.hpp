4f224a575ee757149b6961cfa7dd1c75b3750f65
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-02-22 18:43:41 -0600

    removed debug context

--- addons/vehicleCoasting/script_component.hpp
+++ addons/vehicleCoasting/script_component.hpp
@@ -2,9 +2,9 @@
 #define COMPONENT_BEAUTIFIED Vehicle_Coasting
 #include "\z\potato\addons\core\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
-#define DEBUG_MODE_DRAW_EH
-#define DISABLE_COMPILE_CACHE
+// #define DEBUG_MODE_FULL
+// #define DEBUG_MODE_DRAW_EH
+// #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
 #define COAST_LOOP_INTERVAL 0.2
