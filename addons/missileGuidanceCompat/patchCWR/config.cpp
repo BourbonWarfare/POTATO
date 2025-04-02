2b83d20e5b53556e96afaf1d7b7e0fbd440a24c4
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-04-02 00:19:11 -0500

    temporary fix for missing missile

--- addons/missileGuidanceCompat/patchCWR/config.cpp
+++ addons/missileGuidanceCompat/patchCWR/config.cpp
@@ -7,7 +7,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"potato_missileGuidanceCompat", "cwr3_weapon_config"};
+        requiredAddons[] = {"potato_missileGuidanceCompat", "gm_weapons_launchers_fim43", "cwr3_weapon_config"};
         skipWhenMissingDependencies = 1;
         author = "Bourbon Warfare";
         authorUrl = "https://github.com/BourbonWarfare/POTATO";
@@ -19,6 +19,6 @@ class CfgPatches {
 class CfgMagazines {
     class CA_LauncherMagazine;
     class cwr3_redeye_m: CA_LauncherMagazine {
-        ammo = QEGVAR(missileGuidanceCompat,redeye);
+        ammo = "gm_rocket_70mm_HE_m585";
     };
 };
