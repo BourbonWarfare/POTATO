ea7861872bf94466e342dbe73f26069b58000ebd
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-04-12 14:43:06 -0500

    increased damage by round

--- addons/aceEdits/CfgAmmo.hpp
+++ addons/aceEdits/CfgAmmo.hpp
@@ -6,18 +6,22 @@ class CfgAmmo {
     class ammo_Bomb_SDB;
     class ACEGVAR(missile_sdb,sdb): ammo_Bomb_SDB {
         ACEGVAR(frag,skip) = 0;
+        ACEGVAR(frag,force) = 1;
         ACEGVAR(frag,charge) = 70; // kg guess - 93 kg total
         ACEGVAR(frag,metal) = 16; // kg
         // Wild guess, they slow det velocity to reduce increase deltaP
         ACEGVAR(frag,gurney_c) = 2600;
         ACEGVAR(frag,gurney_k) = 3/5;
         class ace_missileguidance;
+        indirectHit = 300; // 10m probably kills from docs
+        indirectHitRange = 5;
     };
     class GVAR(lsdb): ACEGVAR(missile_sdb,sdb) {
         class ace_missileguidance: ace_missileguidance {
             enabled = 1;
             defaultSeekerType = "MULTI";
             seekerTypes[] = {"MULTI", "SALH", "GPS"};
+            lineGainD = 10;
         };
     };
 };
