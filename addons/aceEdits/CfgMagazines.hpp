e41e3fecfe5baf40982f81ffb9d7e283a89ef3bc
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-04-11 23:56:18 -0500

    Added mass to SDB, and expanded pylons capabilities

--- addons/aceEdits/CfgMagazines.hpp
+++ addons/aceEdits/CfgMagazines.hpp
@@ -1,14 +1,34 @@
 class CfgMagazines {
-    class ACEGVAR(missile_sdb,magazine_bomb_SDB_x1);
-    class ACEGVAR(missile_sdb,PylonRack_bomb_SDB_x4);
+    class magazine_Bomb_SDB_x1;
+    class ACEGVAR(missile_sdb,magazine_bomb_SDB_x1): magazine_Bomb_SDB_x1 {};
+    class ACEGVAR(missile_sdb,PylonRack_bom_SDB_x1): ACEGVAR(missile_sdb,magazine_bomb_SDB_x1) {
+        // Add capabilities to CUP and other mods (can look jank)
+        hardpoints[] = {"B_BOMB_PYLON", "B_SDB_QUAD_RAIL"};
+        mass = 129; // Pylons treat mass as kg
+        model = "\A3\Weapons_F\DynamicLoadout\PylonPod_1x_Missile_AGM_02_F.p3d";
+    };
     class GVAR(magazine_bomb_LSDB_x1): ACEGVAR(missile_sdb,magazine_bomb_SDB_x1) {
-        displayName = "1x GBU-39B/B LSDB [POT]";
         ammo = QGVAR(lsdb);
+        author = "Lambda.Tiger";
+        displayName = "1x GBU-39B/B LSDB [POT]";
+        displayNameShort = "GBU-39B/B [POT]";
+    };
+    class GVAR(PylonRack_bomb_LSDB_x1): GVAR(magazine_bomb_LSDB_x1) {
+        hardpoints[] = {"B_BOMB_PYLON", "B_SDB_QUAD_RAIL"};
+        mass = 129;
+        model = "\A3\Weapons_F\DynamicLoadout\PylonPod_1x_Missile_AGM_02_F.p3d";
     };
 
+    class PylonRack_Bomb_SDB_x4: magazine_Bomb_SDB_x1 {
+        hardpoints[] = {"B_BOMB_PYLON", "B_SDB_QUAD_RAIL"};
+        mass = 550;
+    };
+    class ACEGVAR(missile_sdb,PylonRack_bomb_SDB_x4): PylonRack_Bomb_SDB_x4 {};
     class GVAR(PylonRack_bomb_LSDB_x4): ACEGVAR(missile_sdb,PylonRack_bomb_SDB_x4) {
-        displayName = "4x GBU-39B/B LSDB [POT]";
         ammo = QGVAR(lsdb);
+        author = "Lambda.Tiger";
+        displayName = "4x GBU-39B/B LSDB [POT]";
+        displayNameShort = "GBU-39B/B [POT]";
         pylonWeapon = QGVAR(lsdb);
     };
 };
