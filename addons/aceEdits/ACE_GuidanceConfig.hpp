efce18a3d1aa1969a4def7fa8dd71aa2dc346830
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-04-11 23:56:24 -0500

    fix laser code

--- addons/aceEdits/ACE_GuidanceConfig.hpp
+++ addons/aceEdits/ACE_GuidanceConfig.hpp
@@ -2,6 +2,6 @@ class ACEGVAR(missileGuidance,SeekerTypes) {
     class GPS;
     class MULTI: GPS {
         functionName = QFUNC(seekerType_multi); // didn't want to prep functions here
-        hudInfo = QACEGVAR(missile_hud,info_salh); // SALH more important than GPS info
+        hudInfo = QACEFUNC(missile_hud,info_salh); // SALH more important than GPS info
     };
 };
