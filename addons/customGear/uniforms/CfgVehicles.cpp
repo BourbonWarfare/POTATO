ab011f5d8f08976758dd98fd487fcffe2266bae9
AChesheireCat <paraglader@gmail.com>
2025-04-24 21:15:35 -0400

    adds retextured uniforms
    Finally got around to adding some retextured uniforms that I had been working on.
    
    - Adds a CWRIII dependent Czechoslovakian uniform in "Jehlici" camouflage
    
    - Adds PTV2.0 dependent Finnish uniforms in M08 temperate and winter camouflage
    
    - Adds a CUP dependent Crye G3 uniform in MARPAT Woodland

---
+++ addons/customGear/uniforms/CfgVehicles.cpp
@@ -0,0 +1,198 @@
+class CfgVehicles {
+    class Man;
+    class CAManBase: Man {};
+    class SoldierWB: CAManBase {};
+    class B_Soldier_base_F;
+    class csla_jehlici_uniform_base: B_Soldier_base_F {
+        model = "\cwr3\humans\cwr3_soldiers_rus\uniforms\cwr3_uniform_m69.p3d";
+		scope = 1;
+		scopeCurator = 1;
+		uniformClass = "csla_jehlici";
+        hiddenSelections[] = {
+            "hs_camo1",
+            "hs_camo2",
+            "hs_camo3",
+            "insignia"
+        };
+        hiddenSelectionsTextures[] = {
+            QPATHTOEF(customGear,uniforms\data\czech_needle_uniform_1_co.paa),
+            QPATHTOEF(customGear,uniforms\data\czech_needle_uniform_2_co.paa),
+		    "",
+		    "",
+		    ""
+        };
+    };
+    class csla_jehlici_uniform_tan: csla_jehlici_uniform_base {
+		uniformClass = "csla_tan_jehlici";
+        hiddenSelections[] = {
+            "hs_camo1",
+            "hs_camo2",
+            "hs_camo3",
+            "insignia"
+        };
+        hiddenSelectionsTextures[] = {
+            QPATHTOEF(customGear,uniforms\data\czech_needle_brown_uniform_1_co.paa),
+            QPATHTOEF(customGear,uniforms\data\czech_needle_brown_uniform_2_co.paa),
+		    "",
+		    "",
+		    ""
+        };
+    };
+    
+    // PTV 2.0 retextures
+    
+    class ptv_s_m08: B_Soldier_base_F {};
+    class potato_s_g3cw_serp_m05wdl_v2: ptv_s_m08 {
+        author = "AMF/VityaB/Siege-A & PTV/Chesheire";
+        scope = 2;
+        displayName = "Särmä TST L6 Hardshell Jacket (M05 Wdl) v2";
+        model = "\ptv_characters\uniforms\ptv_Uniform_07.p3d";
+        modelSides[] = {3,1};
+        nakedUniform = "U_BasicBody";
+        uniformClass = "potato_u_g3cw_serp_m05wdl_v2";
+        hiddenSelections[] = {
+            "Camo1",
+            "Camo2",
+            "Camo3",
+            "Camo4",
+            "noglove",
+            "grade",
+            "sang",
+            "drapeau",
+            "insignia",
+            "clan"
+        };
+        hiddenSelectionsTextures[] = {
+            QPATHTOEF(customGear,uniforms\data\ptv_jacket_m05_v2_co.paa),
+            QPATHTOEF(customGear,uniforms\data\G3_PANT_M05_v2_CO.paa),
+            "\ptv_characters\uniforms\Data\m77_co.paa",
+            "\ptv_characters\uniforms\data\usp_gloves_co.paa",
+            "",
+            ""
+        };
+        hiddenSelectionsMaterials[] = {"\ptv_characters\uniforms\data\jacket_summer.rvmat"};
+    };
+    class potato_s_g3cw_serp_m05wdl_v3: ptv_s_m08 {
+        author = "AMF/VityaB/Siege-A & PTV/Chesheire";
+        scope = 2;
+        displayName = "Särmä TST L6 Hardshell Jacket (M05 Wdl) v3";
+        model = "\ptv_characters\uniforms\ptv_Uniform_07.p3d";
+        modelSides[] = {3,1};
+        nakedUniform = "U_BasicBody";
+        uniformClass = "potato_u_g3cw_serp_m05wdl_v3";
+        hiddenSelections[] = {
+            "Camo1",
+            "Camo2",
+            "Camo3",
+            "Camo4",
+            "noglove",
+            "grade",
+            "sang",
+            "drapeau",
+            "insignia",
+            "clan"
+        };
+        hiddenSelectionsTextures[] = {
+            QPATHTOEF(customGear,uniforms\data\ptv_jacket_m05_v3_co.paa),
+            QPATHTOEF(customGear,uniforms\data\G3_PANT_M05_v2_CO.paa),
+            "\ptv_characters\uniforms\Data\m77_co.paa",
+            "\ptv_characters\uniforms\data\usp_gloves_co.paa",
+            "",
+            ""
+        };
+        hiddenSelectionsMaterials[] = {"\ptv_characters\uniforms\data\jacket_summer.rvmat"};
+    };
+    class potato_s_g3cw_serp_m05wdlwint: ptv_s_m08 {
+        author = "AMF/VityaB/Siege-A & PTV/Chesheire";
+        scope = 2;
+        displayName = "Särmä TST L6 Hardshell Jacket (M05 Wdl/Wint)";
+        model = "\ptv_characters\uniforms\ptv_Uniform_07.p3d";
+        modelSides[] = {3,1};
+        nakedUniform = "U_BasicBody";
+        uniformClass = "potato_u_g3cw_serp_m05wdlwint";
+        hiddenSelections[] = {
+            "Camo1",
+            "Camo2",
+            "Camo3",
+            "Camo4",
+            "noglove",
+            "grade",
+            "sang",
+            "drapeau",
+            "insignia",
+            "clan"
+        };
+        hiddenSelectionsTextures[] = {
+            QPATHTOEF(customGear,uniforms\data\ptv_jacket_m05_v2_co.paa),
+            QPATHTOEF(customGear,uniforms\data\G3_PANT_M05Wint_CO.paa),
+            "\ptv_characters\uniforms\Data\m77_co.paa",
+            "\ptv_characters\uniforms\data\usp_gloves_co.paa",
+            "",
+            ""
+        };
+        hiddenSelectionsMaterials[] = {"\ptv_characters\uniforms\data\jacket_summer.rvmat"};
+    };
+    class potato_s_g3cw_serp_m05wdlwint_v2: ptv_s_m08 {
+        author = "AMF/VityaB/Siege-A & PTV/Chesheire";
+        scope = 2;
+        displayName = "Särmä TST L6 Hardshell Jacket (M05 Wdl/Wint) v2";
+        model = "\ptv_characters\uniforms\ptv_Uniform_07.p3d";
+        modelSides[] = {3,1};
+        nakedUniform = "U_BasicBody";
+        uniformClass = "potato_u_g3cw_serp_m05wdlwint_v2";
+        hiddenSelections[] = {
+            "Camo1",
+            "Camo2",
+            "Camo3",
+            "Camo4",
+            "noglove",
+            "grade",
+            "sang",
+            "drapeau",
+            "insignia",
+            "clan"
+        };
+        hiddenSelectionsTextures[] = {
+            QPATHTOEF(customGear,uniforms\data\ptv_jacket_m05_v3_co.paa),
+            QPATHTOEF(customGear,uniforms\data\G3_PANT_M05Wint_v2_CO.paa),
+            "\ptv_characters\uniforms\Data\m77_co.paa",
+            "\ptv_characters\uniforms\data\usp_gloves_co.paa",
+            "",
+            ""
+        };
+        hiddenSelectionsMaterials[] = {"\ptv_characters\uniforms\data\jacket_summer.rvmat"};
+    };
+    
+    // CUP Crye G3 retextures
+    
+    class CUP_Creatures_Military_USMC_Soldier_Base: SoldierWB {};
+    class CUP_B_USMC_Soldier_02: CUP_Creatures_Military_USMC_Soldier_Base {};
+    class potato_CRYE_G3C_MARWDL_v8_US: CUP_B_USMC_Soldier_02 { // I think it's this one that's the best one iirc
+		author = "AChesheireCat";
+		scope = 1;
+		scopeCurator = 0;
+		model = "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_USMC\CUP_U_crye_g3c.p3d";
+		uniformClass = "potato_U_CRYE_G3C_MARWDL_v8_US";
+		hiddenSelections[] = {"Camo","Camo1","Camo2","Camo4","insignia"};
+		hiddenSelectionsTextures[] = {
+            QPATHTOEF(customGear,uniforms\data\gen3_marpatw_v8_us_co.paa),
+            QPATHTOEF(customGear,uniforms\data\gen3_marpatw_v8_us_co.paa),
+            "CUP\Creatures\People\Military\CUP_Creatures_People_Military_USMC\data\g3\fer_blk_co.paa",
+            "CUP\Creatures\People\Military\CUP_Creatures_People_Military_USMC\data\g3\Mechanix_tan_co.paa"
+        };
+	};
+    class potato_CRYE_G3C_MARWDL_v9_US: CUP_B_USMC_Soldier_02 {
+		author = "AChesheireCat";
+		scope = 1;
+		scopeCurator = 0;
+		model = "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_USMC\CUP_U_crye_g3c.p3d";
+		uniformClass = "potato_U_CRYE_G3C_MARWDL_v9_US";
+		hiddenSelections[] = {"Camo","Camo1","Camo2","Camo4","insignia"};
+		hiddenSelectionsTextures[] = {
+            QPATHTOEF(customGear,uniforms\data\gen3_marpatw_v9_us_co.paa),
+            QPATHTOEF(customGear,uniforms\data\gen3_marpatw_v9_us_co.paa),
+            "CUP\Creatures\People\Military\CUP_Creatures_People_Military_USMC\data\g3\fer_blk_co.paa",
+            "CUP\Creatures\People\Military\CUP_Creatures_People_Military_USMC\data\g3\Mechanix_tan_co.paa"
+        };
+	};
+};
---
+++ addons/customGear/uniforms/CfgWeapons.cpp
@@ -0,0 +1,180 @@
+class CfgWeapons {
+    class InventoryItem_Base_F;
+    class ItemCore;
+    class Uniform_Base;
+    class HeadGearItem;
+    class UniformItem;
+    
+    class jasons_indfor_uniform_base: ItemCore {
+        allowedSlots[] = {901};
+        author = "Colonel Jason";
+        displayName = "Jason INDFOR Uniform Base";
+        dlc="";
+		model = "\a3\characters_f\common\suitpacks\suitpack_universal_f.p3d";
+		picture = "";
+		hiddenSelections[]={"camo"};
+		scope = 1;
+		scopeCurator = 1;
+		scopeArsenal = 1; 
+        class ItemInfo : UniformItem {
+            containerClass = "Supply40";
+		    mass = 20;
+		    uniformClass = "";
+		    uniformModel = "-";
+        };
+    };
+    
+    class csla_jehlici: jasons_indfor_uniform_base {
+        displayName = "CZE M69 (Jehlici)";
+		picture = "\cwr3\humans\cwr3_soldiers_rus\ui\cwr3_u_m82_ca.paa";
+		hiddenSelections[] = {"camo"};
+        hiddenSelectionsTextures[] = {
+            QPATHTOEF(customGear,uniforms\data\czech_needle_uniform_1_co.paa)
+        };
+        scope = 2;          
+		scopeCurator = 1;
+		scopeArsenal = 2;
+        class ItemInfo : UniformItem {
+            containerClass = "Supply40";
+		   mass = 20;
+		   uniformClass = "csla_jehlici_uniform_base"; 
+		   uniformModel = "-";
+        };
+    };
+    
+    class csla_tan_jehlici: csla_jehlici {
+        displayName = "CZE M69 (Jehlici,Tan)";
+        hiddenSelections[] = {"camo"};
+        hiddenSelectionsTextures[] = {
+            QPATHTOEF(customGear,uniforms\data\czech_needle_brown_uniform_1_co.paa)
+        };
+        scope = 2;          
+		scopeCurator = 1;
+		scopeArsenal = 2;
+        class ItemInfo : UniformItem {
+            containerClass = "Supply40";
+		   mass = 20;
+		   uniformClass = "csla_jehlici_uniform_tan"; 
+		   uniformModel = "-";
+        };
+    };
+    
+    // PTV 2.0 retextures
+    
+    class ptv_u_m08: Uniform_Base {};
+    class potato_u_g3cw_serp_m05wdl_v2: ptv_u_m08 {
+		author = "HOBO/LYYAMAO/AMF & PTV/Chesheire";
+		scope = 2;
+		displayName = "Särmä TST L6 Hardshell Jacket (M05 Wdl) v2";
+		picture = "\ptv_characters\data\ui\icon_u_ptv_uniform_03_ca.paa";
+		model = "\ptv_characters\uniforms\ptv_suitpack_uni_02";
+		hiddenSelections[] = {
+            "camo",
+            "camo2"
+        };
+		hiddenSelectionsTextures[] = {
+            "\ptv_characters\uniforms\data\G3_SHIRT_SERP_CO.paa",
+            QPATHTOEF(customGear,uniforms\data\G3_PANT_M05_v2_CO.paa)
+        };
+		class ItemInfo: UniformItem {
+			uniformModel = "-";
+			uniformClass = "potato_s_g3cw_serp_m05wdl_v2";
+			containerClass = "Supply40";
+			mass = 40;
+		};
+	};
+    class potato_u_g3cw_serp_m05wdl_v3: ptv_u_m08 {
+		author = "HOBO/LYYAMAO/AMF & PTV/Chesheire";
+		scope = 2;
+		displayName = "Särmä TST L6 Hardshell Jacket (M05 Wdl) v3";
+		picture = "\ptv_characters\data\ui\icon_u_ptv_uniform_03_ca.paa";
+		model = "\ptv_characters\uniforms\ptv_suitpack_uni_02";
+		hiddenSelections[] = {
+            "camo",
+            "camo2"
+        };
+		hiddenSelectionsTextures[] = {
+            "\ptv_characters\uniforms\data\G3_SHIRT_SERP_CO.paa",
+            QPATHTOEF(customGear,uniforms\data\G3_PANT_M05_v2_CO.paa)
+        };
+		class ItemInfo: UniformItem {
+			uniformModel = "-";
+			uniformClass = "potato_s_g3cw_serp_m05wdl_v3";
+			containerClass = "Supply40";
+			mass = 40;
+		};
+	};
+    class potato_u_g3cw_serp_m05wdlwint: ptv_u_m08 {
+		author = "HOBO/LYYAMAO/AMF & PTV/Chesheire";
+		scope = 2;
+		displayName = "Särmä TST L6 Hardshell Jacket (M05 Wdl/Wint)";
+		picture = "\ptv_characters\data\ui\icon_u_ptv_uniform_03_ca.paa";
+		model = "\ptv_characters\uniforms\ptv_suitpack_uni_02";
+		hiddenSelections[] = {
+            "camo",
+            "camo2"
+        };
+		hiddenSelectionsTextures[] = {
+            "\ptv_characters\uniforms\data\G3_SHIRT_SERP_CO.paa",
+            QPATHTOEF(customGear,uniforms\data\G3_PANT_M05Wint_CO.paa)
+        };
+		class ItemInfo: UniformItem {
+			uniformModel = "-";
+			uniformClass = "potato_s_g3cw_serp_m05wdlwint";
+			containerClass = "Supply40";
+			mass = 40;
+		};
+	};
+    class potato_u_g3cw_serp_m05wdlwint_v2: ptv_u_m08 {
+		author = "HOBO/LYYAMAO/AMF & PTV/Chesheire";
+		scope = 2;
+		displayName = "Särmä TST L6 Hardshell Jacket (M05 Wdl/Wint) v2";
+		picture = "\ptv_characters\data\ui\icon_u_ptv_uniform_03_ca.paa";
+		model = "\ptv_characters\uniforms\ptv_suitpack_uni_02";
+		hiddenSelections[] = {
+            "camo",
+            "camo2"
+        };
+		hiddenSelectionsTextures[] = {
+            "\ptv_characters\uniforms\data\G3_SHIRT_SERP_CO.paa",
+            QPATHTOEF(customGear,uniforms\data\G3_PANT_M05Wint_v2_CO.paa)
+        };
+		class ItemInfo: UniformItem {
+			uniformModel = "-";
+			uniformClass = "potato_s_g3cw_serp_m05wdlwint";
+			containerClass = "Supply40";
+			mass = 40;
+		};
+	};
+    
+    // CUP Crye G3 retextures
+    
+    class potato_U_CRYE_G3C_MARWDL_v8_US: ItemCore { // I think it's this one that's the best one iirc
+		author = "AChesheireCat";
+		scope = 2;
+		allowedSlots[] = {901};
+		displayName = "Crye Gen 3  (MARPAT Woodland v8)";
+		picture = "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_USArmy\data\ui\icon_CUP_CRYE_V1_Full.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		class ItemInfo: UniformItem {
+			uniformModel = "-";
+			uniformClass = "potato_CRYE_G3C_MARWDL_v8_US";
+			containerClass = "Supply40";
+			mass = 20;
+		};
+	};
+    class potato_U_CRYE_G3C_MARWDL_v9_US: ItemCore {
+		author = "AChesheireCat";
+		scope = 2;
+		allowedSlots[] = {901};
+		displayName = "Crye Gen 3  (MARPAT Woodland v9)";
+		picture = "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_USArmy\data\ui\icon_CUP_CRYE_V1_Full.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+		class ItemInfo: UniformItem {
+			uniformModel = "-";
+			uniformClass = "potato_CRYE_G3C_MARWDL_v9_US";
+			containerClass = "Supply40";
+			mass = 20;
+		};
+	};
+};
