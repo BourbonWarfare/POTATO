cbdbd05071bfccc62af57abaf79924a51b8d3608
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-05-29 22:12:03 -0500

    updateClassnames

--- addons/missileGuidanceCompat/patchGM/ACE_CSW_Groups.hpp
+++ addons/missileGuidanceCompat/patchGM/ACE_CSW_Groups.hpp
@@ -1,10 +1,10 @@
 class ACE_CSW_Groups {
     //// MILAN
-    class gm_1Rnd_milan_heat_dm82_csw {
+    class ACEGVAR(compat_gm_csw,gm_1Rnd_milan_heat_dm82) {
         delete gm_1Rnd_milan_heat_dm82;
         gm_1Rnd_milan_heat_dm82_ace_mg = 1;
     };
-    class gm_1Rnd_milan_heat_dm92_csw {
+    class ACEGVAR(compat_gm_csw,gm_1Rnd_milan_heat_dm92) {
         delete gm_1Rnd_milan_heat_dm92;
         gm_1Rnd_milan_heat_dm92_ace_mg = 1;
     };
