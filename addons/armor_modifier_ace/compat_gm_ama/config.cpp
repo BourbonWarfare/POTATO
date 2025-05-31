514ef35ce1ba32abd257593eb7ddbefe2e4c5151
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-05-30 23:34:02 -0500

    Added fix for armored GM vests

---
+++ addons/armor_modifier_ace/compat_gm_ama/config.cpp
@@ -0,0 +1,22 @@
+#include "\z\potato\addons\armor_modifier_ace\script_component.hpp"
+#undef COMPONENT
+#define COMPONENT compat_gm_ama
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "potato_armor_modifier_ace",
+            "gm_characters_gc_characters",
+            "gm_characters_ge_characters"
+        };
+        skipWhenMissingDependencies = 1;
+        author = "Bourbon Warfare";
+        authorUrl = "https://github.com/BourbonWarfare/POTATO";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgWeapons.hpp"
