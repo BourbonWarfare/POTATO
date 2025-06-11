4cd72f25b816e1243b1fa4c37ef1b89fa29b8cac
AChesheireCat <paraglader@gmail.com>
2025-06-11 17:07:24 -0400

    tweak #3

--- addons/miscFixes/patchCUP/config.cpp
+++ addons/miscFixes/patchCUP/config.cpp
@@ -434,7 +434,7 @@ class CfgWeapons {
     class CUP_launch_M136_Loaded: Launcher_Base_F {
         ace_overpressure_angle = 45;
         ace_overpressure_damage = 0.5;
-        ace_overpressure_range = 50; // increasing range due to reduction in "lethal" damage - original 10m was described as "lethal"
+        ace_overpressure_range = 50; // increasing range due to reduction in "lethal" damage - original 10m was described as "lethal", irl total backblast range listed as 100m
     };
 };
 
