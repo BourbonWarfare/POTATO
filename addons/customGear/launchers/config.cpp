2cdb83e1626a863417a9e8145e29e51bd70adb57
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-06-13 21:43:44 -0500

    Jank ranging version of more M72 and AT4 series

--- addons/customGear/launchers/config.cpp
+++ addons/customGear/launchers/config.cpp
@@ -1,6 +1,9 @@
 #include "\z\potato\addons\customGear\script_component.hpp"
 #undef COMPONENT
 #define COMPONENT customGear_launchers
+#define WEAPONS_PATCHES(var1) QWEAPON(TRIPLES(launch,var1,Loaded)),\
+QWEAPON(DOUBLES(launch,var1)),\
+QWEAPON(TRIPLES(launch,var1,Used))
 
 class CfgPatches {
     class ADDON {
@@ -11,19 +14,23 @@ class CfgPatches {
             QWEAPON(launch_M136A1_CS_Used),
             QWEAPON(launch_M72A9_Loaded),
             QWEAPON(launch_M72A9),
-            QWEAPON(launch_M72A9_Used)
+            QWEAPON(launch_M72A9_Used),
+            WEAPONS_PATCHES(AT4CS_LMAW),
+            WEAPONS_PATCHES(AT4CS_HP),
+            WEAPONS_PATCHES(M72A7),
+            WEAPONS_PATCHES(M72A8),
+            WEAPONS_PATCHES(M72A10)
         };
         magazines[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = { "potato_core", "CUP_Weapons_LoadOrder"};
         skipWhenMissingDependencies = 1;
         author = "Potato";
-        authors[] = {"AChesheireCat"};
+        authors[] = {"AChesheireCat", "Lambda.Tiger"};
         authorUrl = "https://github.com/BourbonWarfare/POTATO";
         VERSION_CONFIG;
     };
 };
-
 
 #include "CfgAmmo.hpp"
 #include "CfgDisposableLaunchers.hpp"
