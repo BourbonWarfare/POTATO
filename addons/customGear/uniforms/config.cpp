82419ed164356cdd1dfa0006f9eaebf2a32dcfa5
AChesheireCat <66699749+AChesheireCat@users.noreply.github.com>
2025-06-26 20:48:50 -0400

    Uniform Retextures (#694)
    * adds retextured uniforms
    
    Finally got around to adding some retextured uniforms that I had been working on.
    
    - Adds a CWRIII dependent Czechoslovakian uniform in "Jehlici" camouflage
    
    - Adds PTV2.0 dependent Finnish uniforms in M08 temperate and winter camouflage
    
    - Adds a CUP dependent Crye G3 uniform in MARPAT Woodland
    
    * Fixed file extension mistake
    
    + fixed PTV winter v2 pathing to the wrong pants texture
    
    * Update addons/customGear/uniforms/CfgVehicles.hpp
    
    Co-authored-by: lambdatiger <spam.stnuocca.sl@gmail.com>
    
    * Update addons/customGear/uniforms/CfgVehicles.hpp
    
    Co-authored-by: lambdatiger <spam.stnuocca.sl@gmail.com>
    
    * Classname Prefix Change
    
    Swapped the prefixs from already occupied ones (CSLA) to potato.
    
    * Update addons/customGear/uniforms/config.cpp
    
    Co-authored-by: PabstMirror <pabstmirror@gmail.com>
    
    * CfgVehicles Scope
    
    Fixes Scope values on uniforms in CfgVehicles
    
    ---------
    
    Co-authored-by: lambdatiger <spam.stnuocca.sl@gmail.com>
    Co-authored-by: PabstMirror <pabstmirror@gmail.com>

---
+++ addons/customGear/uniforms/config.cpp
@@ -0,0 +1,28 @@
+#include "\z\potato\addons\customGear\script_component.hpp"
+#undef COMPONENT
+#define COMPONENT customGear_uniforms
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {
+            "potato_jehlici",
+            "potato_tan_jehlici",
+            "potato_u_g3cw_serp_m05wdl_v2",
+            "potato_u_g3cw_serp_m05wdl_v3",
+            "potato_u_g3cw_serp_m05wdlwint",
+            "potato_u_g3cw_serp_m05wdlwint_v2",
+            "potato_U_CRYE_G3C_MARWDL_v8_US",
+            "potato_U_CRYE_G3C_MARWDL_v9_US"
+        };
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = { "potato_core", "cwr3_soldiers_us", "ptv_characters", "ptv_characters_cfg", "CUP_Weapons_LoadOrder", "CUP_Creatures_People_LoadOrder" };
+        skipWhenMissingDependencies = 1;
+        author = "Bourbon Warfare";
+        authorUrl = "https://github.com/BourbonWarfare/POTATO";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgWeapons.hpp"
+#include "CfgVehicles.hpp"
