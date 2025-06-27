498e71b469fabad626a95840fb1a833ae4e488bb
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-06-26 20:06:18 -0500

    attempt to override video settings changes made to vanilla

---
+++ addons/miscFixes/patchPhoton/CfgVideoOptions.hpp
@@ -0,0 +1,14 @@
+
+class CfgVideoOptions {
+    class Particles {
+        class Low {
+            smokeZoomCoef = 1.42857;
+        };
+        class Normal {
+            smokeZoomCoef = 1.42857;
+        };
+        class High {
+            smokeZoomCoef = 1.42857;
+        };
+    };
+};
