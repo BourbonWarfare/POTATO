d389010311fb622258bfd7900736935fbefe253f
AChesheireCat <paraglader@gmail.com>
2025-07-11 23:44:27 -0400

    +M4 Carbine RIS ver

--- addons/customGear/fauxC7/CfgWeapons.hpp
+++ addons/customGear/fauxC7/CfgWeapons.hpp
@@ -131,79 +131,6 @@ class CfgWeapons {
             libTextDesc = "The M4 Carbine is a shortened version of the M16A2. It sports a shortened barrel and collapsible stock, making it the Special Operations weapon of choice. As with the M16A2, the M4 Carbine is limited to a 3-round burst mode.&lt;br/&gt;The M4 Carbine can be modified with a variety of special accessories including optics such as the Aimpoint 5000, an easily detachable suppressor, the M203 under barrel grenade launcher, and many others.";
         };
         descriptionShort = "$STR_CUP_dss_m16a2";
-        /*
-        modes[] = {"Single","Burst","Burst_medium","single_medium_optics1","single_medium_optics2"};
-        class Single: Mode_SemiAuto {
-            class BaseSoundModeType{};
-            class StandardSound: BaseSoundModeType {
-                soundsetshot[] = {"jsrs_m16a4_shot_soundset","jsrs_5x56mm_reverb_soundset"};
-            };
-            class SilencedSound: BaseSoundModeType {
-                soundsetshot[] = {"jsrs_spar_shot_silenced_soundset","jsrs_5x56mm_sd_reverb_soundset"};
-            };
-            dispersion = 0.0006;
-            reloadTime = 0.075;
-            minRange = 2;
-            minRangeProbab = 0.5;
-            midRange = 250;
-            midRangeProbab = 0.7;
-            maxRange = 400;
-            maxRangeProbab = 0.3;
-        };
-        class Burst: Mode_Burst {
-            class BaseSoundModeType{};
-            class StandardSound: BaseSoundModeType {
-                soundsetshot[] = {"jsrs_m16a4_shot_soundset","jsrs_5x56mm_reverb_soundset"};
-            };
-            class SilencedSound: BaseSoundModeType {
-                soundsetshot[] = {"jsrs_spar_shot_silenced_soundset","jsrs_5x56mm_sd_reverb_soundset"};
-            };
-            soundBurst = 0;
-            ffCount = 6;
-            dispersion = 0.0006;
-            reloadTime = 0.075;
-            minRange = 0;
-            minRangeProbab = 0.9;
-            midRange = 15;
-            midRangeProbab = 0.7;
-            maxRange = 30;
-            maxRangeProbab = 0.1;
-        };
-        class Burst_medium: Burst {
-            showToPlayer = 0;
-            minRange = 1;
-            minRangeProbab = 0.5;
-            midRange = 75;
-            midRangeProbab = 0.7;
-            maxRange = 100;
-            maxRangeProbab = 0.05;
-            aiRateOfFire = 2;
-        };
-        class single_medium_optics1: Single {
-            requiredOpticType = 1;
-            showToPlayer = 0;
-            minRange = 5;
-            minRangeProbab = 0.2;
-            midRange = 350;
-            midRangeProbab = 0.7;
-            maxRange = 525;
-            maxRangeProbab = 0.3;
-            aiRateOfFire = 5;
-            aiRateOfFireDistance = 525;
-        };
-        class single_medium_optics2: single_medium_optics1 {
-            requiredOpticType = 2;
-            minRange = 100;
-            minRangeProbab = 0.1;
-            midRange = 500;
-            midRangeProbab = 0.7;
-            maxRange = 750;
-            maxRangeProbab = 0.05;
-            aiRateOfFire = 6;
-            aiRateOfFireDistance = 750;
-        };
-    };
-    */
         class WeaponSlotsInfo: WeaponSlotsInfo {
             mass = 66.3;
             class CUP_PicatinnyTopMountM16: CUP_PicatinnyTopMount {
@@ -259,6 +186,71 @@ class CfgWeapons {
                 iconScale = 0.23;
                 iconPicture = "\A3\Weapons_F\Data\UI\attachment_muzzle.paa";
             };
+        };
+    };
+    class potato_arifle_M4RIS: potato_arifle_M4 {
+        displayName = "M4 Carbine RIS";
+        reloadMagazineSound[] = {"CUP\Weapons\CUP_Weapons_M16\data\sfx\M4_Reload",1,1,10};
+		changeFiremodeSound[] = {"A3\sounds_f\weapons\closure\firemode_changer_2",0.25118864,1,20};
+        drySound[] = {"CUP\Weapons\CUP_Weapons_M16\data\sfx\Dry",1,1,35};
+        jsrs_soundeffect = "JSRS2_Distance_Effects_M4";
+        discreteDistance[] = {300};
+		ACE_barrelTwist = 178;
+		ACE_barrelLength = 368;
+		ACE_overheating_mrbs = 3600;
+		ACE_overheating_dispersion = 1;
+		ACE_overheating_slowdownFactor = 1;
+        model = "\CUP\Weapons\CUP_Weapons_M16\CUP_M4A1_BUIS.p3d";
+        picture = "\CUP\Weapons\CUP_Weapons_M16\data\ui\gear_m4a1_buis_x_ca.paa";
+        hiddenSelections[] = {"camo","camo_rail","camo1","camo2","camo3","camo4","camo_lod"};
+        hiddenSelectionsTextures[] = {"CUP\Weapons\CUP_Weapons_M16\data\tigg_m4_co.paa","CUP\Weapons\CUP_Weapons_M16\data\m4_v3_co.paa","CUP\Weapons\CUP_Weapons_M16\data\tigg_m4furniture_co.paa","CUP\Weapons\CUP_Weapons_M16\data\tigg_m4barrel_co.paa","CUP\Weapons\CUP_Weapons_M16\data\tigg_carryhandle_co.paa","CUP\Weapons\CUP_Weapons_WeaponsData\Data\RIS_cover\rail_cover_co.paa","CUP\Weapons\CUP_Weapons_M16\data\m4_co.paa"};
+        class WeaponSlotsInfo {
+            mass = 78;
+            class CUP_PicatinnyTopMountM4: CUP_PicatinnyTopMount {
+                iconPinPoint = "center";
+                iconPosition[] = {0.516,0.227};
+                iconScale = 0.18;
+                iconPicture = "\A3\Weapons_F\Data\UI\attachment_top.paa";
+            };
+            class CUP_PicatinnySideMountM4: CUP_PicatinnySideMount {
+                iconPinPoint = "center";
+                iconPosition[] = {0.33,0.371};
+                iconScale = 0.26;
+                iconPicture = "\A3\Weapons_F\Data\UI\attachment_side.paa";
+            };
+            class CUP_PicatinnyUnderMountM16: CUP_PicatinnyUnderMount {
+                iconPinPoint = "center";
+                iconPosition[] = {0.277,0.676};
+                iconScale = 0.3;
+                iconPicture = "\A3\weapons_f_mark\data\UI\attachment_under.paa";
+            };
+            class MuzzleSlot: asdg_MuzzleSlot_556 {
+                linkProxy = "\A3\data_f\proxies\weapon_slots\MUZZLE";
+                iconPinPoint = "center";
+                iconPosition[] = {0.02,0.375};
+                iconScale = 0.25;
+                iconPicture = "\A3\Weapons_F\Data\UI\attachment_muzzle.paa";
+            };
+            allowedslots[] = {901};
+        };
+        inertia = 0.4;
+        dexterity = 1.6;
+    };
+    class potato_arifle_M4RIS_M203GL: potato_arifle_M4RIS {
+        displayName = "M4 Carbine RIS M203";
+        model = "\CUP\Weapons\CUP_Weapons_M16\CUP_M4A1_GL.p3d";
+        picture = "\CUP\Weapons\CUP_Weapons_M16\data\ui\gear_m4a1_buis_gl_x_ca.paa";
+        uiPicture = "\A3\Weapons_F\Data\UI\icon_gl_CA.paa";
+        hiddenSelections[] = {"camo","camo_rail","camo1","camo2","camo3","camo4","camo_lod","camo_gl"};
+        hiddenSelectionsTextures[] = {"CUP\Weapons\CUP_Weapons_M16\data\tigg_m4_co.paa","CUP\Weapons\CUP_Weapons_M16\data\m4_v3_co.paa","CUP\Weapons\CUP_Weapons_M16\data\tigg_m4furniture_co.paa","CUP\Weapons\CUP_Weapons_M16\data\tigg_m4barrel_co.paa","CUP\Weapons\CUP_Weapons_M16\data\tigg_carryhandle_co.paa","CUP\Weapons\CUP_Weapons_M16\data\tigg_rails_co.paa","CUP\Weapons\CUP_Weapons_M16\data\m4_co.paa","CUP\Weapons\CUP_Weapons_M16\data\tigg_m203_m4_co.paa"};
+        handAnim[] = {"OFP2_ManSkeleton","CUP\Weapons\CUP_Weapons_M16\data\anim\M4GL.rtm"};
+        muzzles[] = {"this","M203"};
+        inertia = 0.5;
+        dexterity = 1.5;
+        aimTransitionSpeed = 1;
+        class WeaponSlotsInfo: WeaponSlotsInfo {
+            mass = 107.9;
+            class CUP_PicatinnyUnderMountM16{};
         };
     };
 };
