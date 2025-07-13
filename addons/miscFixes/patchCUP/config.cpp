5ad7fcdfef3d72b2b880cff8eadf4ead7647b38a
AChesheireCat <paraglader@gmail.com>
2025-07-12 20:35:48 -0400

    muzzle outer switchup

--- addons/miscFixes/patchCUP/config.cpp
+++ addons/miscFixes/patchCUP/config.cpp
@@ -251,22 +251,10 @@ class CfgRecoils {
         temporary = 0.005;
     };
     class GVAR(recoil_SAW_2): recoil_default { // testing weird shit v2
-        muzzleOuter[] = {0.52,0.91,0.35,0.25};
+        muzzleOuter[] = {0.52,0.91,0.29,0.21};
         kickBack[] = {0.015,0.02};
         permanent = 0.03;
         temporary = 0.015;
-    };
-    class GVAR(recoil_SAW_2prone): recoil_default {
-        muzzleOuter[] = {0.1,0.3,0.2,0.2};
-        kickBack[] = {0.01,0.03};
-        permanent = 0.04;
-        temporary = 0.005;
-    };
-    class GVAR(recoil_SAW_2prone_old): recoil_default {
-        muzzleOuter[] = {0.26,0.455,0.175,0.125};
-        kickBack[] = {0.0075,0.01};
-        permanent = 0.015;
-        temporary = 0.005;
     };
     class GVAR(recoil_SAW_3): recoil_default { // 35% reduced vertical and horizontal recoil
         muzzleOuter[] = {0.52,0.91,0.35,0.21};
@@ -370,7 +358,6 @@ class CfgWeapons {
     class CUP_lmg_minimi_railed: CUP_lmg_minimipara {};
     class CUP_lmg_m249_pip2: CUP_lmg_minimi_railed {
         recoil = QGVAR(recoil_SAW_2);
-        recoilProne = QGVAR(recoil_SAW_2prone);
     };
     class CUP_lmg_m249_pip3: CUP_lmg_minimi_railed {
         recoil = QGVAR(recoil_SAW_3);
