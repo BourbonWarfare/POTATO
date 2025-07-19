fe737107e6de0ffd356102e995cc1ab598dc497f
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-07-19 12:55:30 -0500

    Hide reminder potato in arsenal

--- addons/assignGear/CfgMagazines.hpp
+++ addons/assignGear/CfgMagazines.hpp
@@ -2,6 +2,7 @@ class CfgMagazines {
     class potato_potato;
     class GVAR(reminder): potato_potato {
         scope = 1;
+        scopeArsenal = 0;
         descriptionShort = "It's a reminder to check ACE cargo";
         displayName = "Check ACE Cargo";
         displayNameShort = "Check ACE Cargo";
