fc668a7cf4a57ed71ed4fc780c1f4c942dfad356
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-07-19 12:52:01 -0500

    Updated weapons array

--- addons/customGear/fauxC7/config.cpp
+++ addons/customGear/fauxC7/config.cpp
@@ -5,7 +5,7 @@
 
 class CfgPatches {
     class ADDON {
-        weapons[] = { "potato_fauxC7_c7", "potato_fauxC7_c7GL", "potato_fauxC7_c7a2", "potato_fauxC7_c7a2GL", "potato_arifle_M4", "potato_arifle_M4_M203GL", "potato_arifle_M4RIS", "potato_arifle_M4RIS_grip", "potato_arifle_M4RIS_M203GL" };
+        weapons[] = { "potato_fauxC7_c7", "potato_fauxC7_c7GL", "potato_fauxC7_c7a2", "potato_fauxC7_c7a2GL", "potato_arifle_M4", "potato_arifle_M4_M203", "potato_arifle_M4_RIS", "potato_arifle_M4_RIS_grip", "potato_arifle_M4_RIS_M203" };
         units[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"potato_core", "CUP_Weapons_LoadOrder", "jsrs_soundmod_cup_weapons"};
