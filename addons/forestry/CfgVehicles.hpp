3d5fbff23d72e065a23b2c0337bd06e434cf3567
AChesheireCat <paraglader@gmail.com>
2025-09-05 19:24:15 -0400

    Moving To Equipment Menu
    Taking cues from Brush Clearing, moved the "chop tree" option to the ACE_Equipment sub-menu.

--- addons/forestry/CfgVehicles.hpp
+++ addons/forestry/CfgVehicles.hpp
@@ -2,19 +2,20 @@ class CfgVehicles {
     class Man;
     class CAManBase: Man {
         class ACE_SelfActions {
-            class POTATO_Menu {
+            class ACE_Equipment {
+/*
                 displayName = "Forestry";
                 condition = "true";
                 statement = "";
                 icon = "\a3\modules_f\data\hideterrainobjects\icon32_ca.paa";
                 exceptions[] = {"isNotSwimming", "isNotInside", "isNotSitting", "isNotOnLadder", "isNotRefueling"};
-
+*/
                 class Chop_Tree {
-                    displayName = "Chop down tree";
+                    displayName = "Chop Down Tree";
                     icon = QPATHTOF(resources\tree.paa);
                     condition = QUOTE(call FUNC(canChop));
                     statement = QUOTE(call FUNC(doChop));
-                    exceptions[] = {};
+                    exceptions[] = {"isNotSwimming", "isNotInside", "isNotSitting", "isNotOnLadder", "isNotRefueling"};
                 };
 /*
                 class Flatten_Grass  {
