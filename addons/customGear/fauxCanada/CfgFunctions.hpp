d92e4b200364d10ddcad407fe082f9ba14b1dfc2
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-10-02 19:15:29 -0500

    customGear - Add 70s-90s Canadian Vehilce Retextures (#785)

---
+++ addons/customGear/fauxCanada/CfgFunctions.hpp
@@ -0,0 +1,9 @@
+class CfgFunctions {
+    class ADDON {
+        class Smoke {
+            class canonSmoke {
+                file = QUOTE(\z\potato\addons\customGear\fauxCanada\functions\DOUBLES(fnc,canonSmoke).sqf);
+            };
+        };
+    };
+};
---
+++ addons/customGear/fauxCanada/config.cpp
@@ -0,0 +1,46 @@
+#include "\z\potato\addons\customGear\script_component.hpp"
+#undef COMPONENT
+#define COMPONENT customGear_fauxCanada
+
+class CfgPatches {
+    class ADDON {
+        weapons[] = {QGVARMAIN(c5_carry)};
+        units[] = {
+            QGVARMAIN(ca_army_c5_low),
+            QGVARMAIN(ca_army_c5_high),
+            QGVARMAIN(ca_army_iltis),
+            QGVARMAIN(ca_army_m151_c5),
+            QGVARMAIN(ca_army_Leopard_c1),
+            QGVARMAIN(ca_army_m113a1_apc),
+            QGVARMAIN(ca_army_static_tow),
+            QGVARMAIN(ca_army_static_m40a1)
+        };
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "potato_core",
+            "CUP_Weapons_LoadOrder",
+            "CUP_Vehicles_LoadOrder",
+            "cwr3_vehicle_m151",
+            "gm_vehicles_land_tracked_leopard1_ge_army_leopard1",
+            "gm_vehicles_land_wheeled_iltis_ge_army_iltis",
+            "gm_vehicles_land_tracked_m113_dk_army_m113",
+            "static_f_vietnam_c",
+            "wheeled_f_vietnam_03_c",
+            "weapons_v_f_vietnam_c"
+        };
+        skipWhenMissingDependencies = 1;
+        author = "Bourbon Warfare";
+        authors[] = {"Lambda.Tiger"};
+        authorUrl = "https://github.com/BourbonWarfare/POTATO";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgAmmo.hpp"
+#include "CfgFunctions.hpp"
+#include "CfgInsignia.hpp"
+#include "CfgMagazineGroups.hpp"
+#include "CfgMagazines.hpp"
+#include "CfgMagazineWells.hpp"
+#include "CfgVehicles.hpp"
+#include "CfgWeapons.hpp"
