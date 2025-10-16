779c147a5a3214ae121a75349e4efa85a62395f7
AChesheireCat <paraglader@gmail.com>
2025-10-16 16:43:06 -0400

    Removing duplicated define

--- addons/miscFixes/patchCUP/config.cpp
+++ addons/miscFixes/patchCUP/config.cpp
@@ -357,7 +357,6 @@ class CfgWeapons {
     // Ups the ROF from 600 to 652, matching IRL specs
     class CUP_arifle_AK_Base;
     class CUP_arifle_AKS_Base;
-    class CUP_arifle_RPK74;
     class CUP_arifle_AK74: CUP_arifle_AK_Base {
         class FullAuto: Mode_FullAuto {
             reloadTime = 0.092; // was 0.1
