ebbca1f611fe4f5d81fd31ad8e082cecc57ee268
lambdatiger <spam.stnuocca.sl@gmail.com>
2025-11-27 18:55:56 -0600

    Desiable debug output from debug addon

--- addons/debug/script_component.hpp
+++ addons/debug/script_component.hpp
@@ -8,7 +8,7 @@
 
 #include "\z\potato\addons\core\script_macros.hpp"
 
-#define ENABLE_SYSTEMS
-#define ENABLE_NETWORK_MARKERS
+// #define ENABLE_SYSTEMS
+// #define ENABLE_NETWORK_MARKERS
 
 #define DEBUG_EVENT_NETWORK_MARKERS 0
