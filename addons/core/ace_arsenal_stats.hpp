ab605fec86f043a86c47f83593f252f28787b510
PabstMirror <pabstmirror@gmail.com>
2025-12-05 16:56:48 -0600

    Update addons/core/ace_arsenal_stats.hpp
    Co-authored-by: lambdatiger <spam.stnuocca.sl@gmail.com>

--- addons/core/ace_arsenal_stats.hpp
+++ addons/core/ace_arsenal_stats.hpp
@@ -7,8 +7,8 @@ class ace_arsenal_stats {
         displayName = "Recoil";
         showBar = 1;
         showText = 1;
-        barStatement = QUOTE([ARR_2(0, _this)] call FUNC(statRecoil));
-        textStatement = QUOTE([ARR_2(1, _this)] call FUNC(statRecoil));
+        barStatement = QUOTE([ARR_2(0,_this)] call FUNC(statRecoil));
+        textStatement = QUOTE([ARR_2(1,_this)] call FUNC(statRecoil));
         tabs[] = {{0,1}, {}};
     };
 };
