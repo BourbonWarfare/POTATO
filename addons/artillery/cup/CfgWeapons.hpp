e14d85db0e672f6c6d6d4f0695e6fc15a84236cc
lambdatiger <spam.stnuocca.sl@gmail.com>
2026-01-27 20:12:35 -0600

    fiix CUP m119 rounds

--- addons/artillery/cup/CfgWeapons.hpp
+++ addons/artillery/cup/CfgWeapons.hpp
@@ -1,7 +1,14 @@
 class CfgWeapons {
     class mortar_155mm_AMOS;
-    ADD_MAGAZINES_1_WEAPON(mortar_155mm_AMOS,CUP_Vcannon_D30_veh,CUP_30Rnd_122mmHE_D30_M);
-    ADD_MAGAZINES_1_WEAPON(CUP_Vcannon_D30_veh,CUP_Vcannon_M119_veh,CUP_30Rnd_105mmHE_M119_M);
+    class CUP_Vcannon_D30_veh: mortar_155mm_AMOS {
+        magazines[] += {QGVARMAIN(DOUBLES(CUP_30Rnd_122mmHE_D30_M,noFrag)),QGVARMAIN(DOUBLES(CUP_30Rnd_122mmHE_D30_M,noSplash))};
+        magazineWell[]  = {};
+    };
+    class CUP_Vcannon_M119_veh: CUP_Vcannon_D30_veh {
+        magazineWell[]  = {QGVARMAIN(105x372mmR)};
+
+    };
+    //ADD_MAGAZINES_1_WEAPON(CUP_Vcannon_D30_veh,CUP_Vcannon_M119_veh,CUP_30Rnd_105mmHE_M119_M);
     class rockets_230mm_GAT;
     ADD_MAGAZINES_1_WEAPON(rockets_230mm_GAT,CUP_Vmlauncher_MLRS_veh,CUP_12Rnd_MLRS_HE);
     ADD_MAGAZINES_1_WEAPON(rockets_230mm_GAT,CUP_Vmlauncher_GRAD_veh,CUP_40Rnd_GRAD_HE);
