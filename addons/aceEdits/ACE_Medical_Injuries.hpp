5c81fa8c99c571d06ca5ec983bc2e1d69cb1c28f
lambdatiger <11843718+lambdatiger@users.noreply.github.com>
2026-03-15 11:27:46 -0500

    aceEdits - Initial ACE Medical Changes - Removing Bruises (#847)

---
+++ addons/aceEdits/ACE_Medical_Injuries.hpp
@@ -0,0 +1,59 @@
+#define REPLACE_DAMAGE_CLASSES class Laceration {\
+    weighting[] = {{20,10},{4,2},{1,1},{0.3,1},{0,0}};\
+}
+
+class ACE_Medical_Injuries {
+    class damageTypes {
+        class bullet {
+            delete Contusion;
+            REPLACE_DAMAGE_CLASSES;
+        };
+        class grenade {
+            delete Contusion;
+            REPLACE_DAMAGE_CLASSES;
+        };
+        class explosive {
+            delete Contusion;
+            REPLACE_DAMAGE_CLASSES;
+        };
+        class shell {
+            delete Contusion;
+            REPLACE_DAMAGE_CLASSES;
+        };
+        class vehiclecrash {
+            delete Abrasion;
+            REPLACE_DAMAGE_CLASSES;
+            delete Contusion;
+        };
+        class collision {
+            delete Abrasion;
+            delete Contusion;
+            REPLACE_DAMAGE_CLASSES;
+        };
+        class falling {
+            delete Abrasion;
+            delete Contusion;
+            REPLACE_DAMAGE_CLASSES;
+        };
+        class backblast {
+            delete Contusion;
+            REPLACE_DAMAGE_CLASSES;
+        };
+        class punch {
+            delete Contusion;
+            REPLACE_DAMAGE_CLASSES;
+        };
+        class ropeburn {
+            delete Abrasion;
+            REPLACE_DAMAGE_CLASSES;
+        };
+        class burn {
+            delete ThermalBurn;
+            REPLACE_DAMAGE_CLASSES;
+        };
+        class unknown {
+            delete Abrasion;
+            REPLACE_DAMAGE_CLASSES;
+        };
+    };
+};
--- addons/aceEdits/XEH_preInit.sqf
+++ addons/aceEdits/XEH_preInit.sqf
@@ -5,5 +5,9 @@ ADDON = false;
 PREP_RECOMPILE_START;
 #include "XEH_PREP.hpp"
 PREP_RECOMPILE_END;
+["CBA_settingsInitialized", {
+    [{ACEGVAR(medical,const_stableVitalsBloodThreshold) = 4.5;}, 0, 1
+    ] call CBA_fnc_waitAndExecute;
+}]  call CBA_fnc_addEventHandler;
 
 ADDON = true;
