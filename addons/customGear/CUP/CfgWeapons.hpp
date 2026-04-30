b034d1ab47202026b54e97e122c0ec8b957bea8b
lambdatiger <spam.stnuocca.sl@gmail.com>
2026-04-30 00:11:55 -0500

    Add fake WP Monoculars

---
+++ addons/customGear/CUP/CfgWeapons.hpp
@@ -0,0 +1,12 @@
+class CfgWeapons {
+    class CUP_NVG_1PN138;
+    class CUP_NVG_1PN138_WP: CUP_NVG_1PN138 {
+        ACEGVAR(nightvision,colorPreset)[] = {0.0, {0.0, 0.0, 0.0, 0.0}, {1.1, 0.8, 1.9, 0.9}, {1, 1, 6, 0.0}};
+        displayname = "1PN138 (WP)";
+    };
+    class CUP_NVG_HMNVS;
+    class CUP_NVG_HMNVS_WP: CUP_NVG_HMNVS {
+        ACEGVAR(nightvision,colorPreset)[] = {0.0, {0.0, 0.0, 0.0, 0.0}, {1.1, 0.8, 1.9, 0.9}, {1, 1, 6, 0.0}};
+        displayname = "HMNVS (WP)";
+    };
+};
