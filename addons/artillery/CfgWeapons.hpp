538176ef03b365ad816876fb0e28778b8ec6a627
lambdatiger <spam.stnuocca.sl@gmail.com>
2026-09-03 22:49:56 -0500

    clear commented defines

--- addons/artillery/CfgWeapons.hpp
+++ addons/artillery/CfgWeapons.hpp
@@ -4,7 +4,6 @@ class CfgWeapons {
     class RocketPods;
     ADD_MAGAZINES_1_WEAPON(RocketPods,rockets_230mm_GAT,12Rnd_230mm_rockets);
     ADD_MAGAZINES_3_WEAPON(CannonCore,mortar_155mm_AMOS,32Rnd_155mm_Mo_shells,2Rnd_155mm_Mo_guided,4Rnd_155mm_Mo_guided);
-    //ADD_MAGAZINES_2_WEAPON(CannonCore,mortar_82mm,8Rnd_82mm_Mo_shells,8Rnd_82mm_Mo_guided);
     class mortar_82mm: CannonCore {
         magazines[] += {MAGAZINE_PAIRS(8Rnd_82mm_Mo_shells), MAGAZINE_PAIRS(8Rnd_82mm_Mo_guided)};
         class Single1: Mode_SemiAuto {};
